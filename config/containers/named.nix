18eb8a944aebba9a1d9ae35b47769e5fc8a6965b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-14 17:02:12 +0100

    Put the zones key in the correct location

--- config/containers/named.nix
+++ config/containers/named.nix
@@ -4,34 +4,34 @@
     config = {
       services.bind = {
         enable = true;
+        zones = {
+          "darkkirb.de" = {
+            master = false;
+            masters = [
+              "fd00:e621:e621::1"
+            ];
+          };
+          "chir.rs" = {
+            master = false;
+            masters = [
+              "fd00:e621:e621::1"
+            ];
+          };
+          "int.chir.rs" = {
+            master = false;
+            masters = [
+              "fd00:e621:e621::1"
+            ];
+          };
+          "rpz.int.chir.rs" = {
+            master = false;
+            masters = [
+              "fd00:e621:e621::1"
+            ];
+          };
+        };
       };
       system.stateVersion = "21.11";
-      zones = {
-        "darkkirb.de" = {
-          master = false;
-          masters = [
-            "fd00:e621:e621::1"
-          ];
-        };
-        "chir.rs" = {
-          master = false;
-          masters = [
-            "fd00:e621:e621::1"
-          ];
-        };
-        "int.chir.rs" = {
-          master = false;
-          masters = [
-            "fd00:e621:e621::1"
-          ];
-        };
-        "rpz.int.chir.rs" = {
-          master = false;
-          masters = [
-            "fd00:e621:e621::1"
-          ];
-        };
-      };
     };
   };
   networking.firewall.allowedTCPPorts = [ 53 ];
