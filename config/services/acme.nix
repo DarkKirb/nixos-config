601e3c55c1035d93abfb7276fc25bd4f22ca13a6
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-14 18:03:14 +0100

    add a default block in the acme config

--- config/services/acme.nix
+++ config/services/acme.nix
@@ -1,23 +1,21 @@
 { ... }: {
   security.acme = {
     acceptTerms = true;
-    email = "lotte@chir.rs";
+    defaults = {
+      email = "lotte@chir.rs";
+      dnsProvider = "rfc2136";
+      credentialsFile = "/run/secrets/security/acme/dns";
+    };
     certs."darkkirb.de" = {
       domain = "*.darkkirb.de";
       extraDomainNames = [ "darkkirb.de" ];
-      dnsProvider = "rfc2136";
-      credentialsFile = "/run/secrets/security/acme/dns";
     };
     certs."chir.rs" = {
       domain = "*.chir.rs";
       extraDomainNames = [ "chir.rs" ];
-      dnsProvider = "rfc2136";
-      credentialsFile = "/run/secrets/security/acme/dns";
     };
     certs."int.chir.rs" = {
       domain = "*.int.chir.rs";
-      dnsProvider = "rfc2136";
-      credentialsFile = "/run/secrets/security/acme/dns";
     };
   };
 }
