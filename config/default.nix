50d902b04bd146230ebb4edcb75586aeeb270892
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-14 10:26:37 +0100

    Add nix flake config

--- config/default.nix
+++ config/default.nix
@@ -1,5 +1,11 @@
-{ ... }: {
+{ pkgs, ... }: {
   imports = [
     ./zfs.nix
   ];
+  nix = {
+    package = pkgs.nixUnstable; # or versioned attributes like nix_2_4
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+  };
 }
