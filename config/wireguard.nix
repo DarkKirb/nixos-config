a0ba9c6b019ea2588fcdf09dd0331bde49563361
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-14 15:47:37 +0100

    Add ubuntu-4gb-fsn1-1 peer

--- config/wireguard.nix
+++ config/wireguard.nix
@@ -5,6 +5,16 @@
     interfaces."wg0" = {
       listenPort = 51820;
       privateKeyFile = "/run/secrets/network/wireguard/privkey";
+      peers = [
+        # Old infra: ubuntu-4gb-fsn1-1
+        {
+          publicKey = "ZtU2iWwVYeGyXC1ak+wFdTuisQNq7gMthYQZaw6InDU=";
+          endpoint = "23.88.44.119:51820";
+          allowedIPs = [
+            "fd00:e621:e621::/48"
+          ];
+        }
+      ];
     };
   };
 }
