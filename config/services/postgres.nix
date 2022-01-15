1faa336925982e047ced8e6913bd655da0a034ef
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-15 21:58:11 +0100

    Add postgresql backup

--- config/services/postgres.nix
+++ config/services/postgres.nix
@@ -4,6 +4,10 @@
     enableTCPIP = true;
     authentication = "host  all all fd0d:a262:1fa6:e621::/64 md5";
   };
+  services.postgresqlBackup = {
+    enable = true;
+    compression = "none"; # the file system and restic both do compression
+  };
   services.prometheus.exporters.postgres = {
     enable = true;
     user = "postgres";
