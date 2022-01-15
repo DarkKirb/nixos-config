3142447046d93016339bf2b02d35b6e7a1bf9beb
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-15 10:57:49 +0100

    Add prometheus node_exporter scraper

--- config/services/prometheus.nix
+++ config/services/prometheus.nix
@@ -1,6 +1,16 @@
-{ ... }: {
+{ config, ... }: {
   services.prometheus = {
     enable = true;
     port = 9001;
+    scrapeConfigs = [
+      {
+        job_name = "node_exporter";
+        static_configs = [{
+          targets = [
+            "${config.services.prometheus.exporters.node.listenAddress}:${toString config.services.prometheus.exporters.node.port}"
+          ];
+        }];
+      }
+    ];
   };
 }
