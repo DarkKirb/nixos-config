8e67606ae86839236ed7eefe0faa373b4f3e1594
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-17 08:02:56 +0100

    Add minio to nginx

--- config/services/minio.nix
+++ config/services/minio.nix
@@ -1,10 +1,29 @@
-{ ... }: {
+{ config, ... }:
+let
+  listenIPs = (import ../../utils/getInternalIP.nix config).listenIPs;
+in
+{
   services.minio = {
     enable = true;
     rootCredentialsFile = "/run/secrets/security/minio/credentials_file";
   };
   services.prometheus.exporters.minio = {
+    # TODO: doesn't work
     enable = true;
   };
-  networking.firewall.interfaces."wg0".allowedTCPPorts = [ 9000 9001 ];
+  services.nginx.virtualHosts."minio.int.chir.rs" = {
+    forceSSL = true;
+    http2 = true;
+    listenAddresses = listenIPs;
+    sslCertificate = "/var/lib/acme/int.chir.rs/cert.pem";
+    sslCertificateKey = "/var/lib/acme/int.chir.rs/key.pem";
+    locations."/" = {
+      proxyPass = "http://127.0.0.1:9000";
+      proxyWebsockets = true;
+    };
+    locations."/web" = {
+      proxyPass = "http://127.0.0.1:9001";
+      proxyWebsockets = true;
+    };
+  };
 }
