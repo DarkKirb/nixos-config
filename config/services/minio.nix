3bf4222598bf932920c35400c8f6703dd67091f5
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-17 08:35:35 +0100

    Make minio console a separate subdomain

--- config/services/minio.nix
+++ config/services/minio.nix
@@ -21,7 +21,14 @@ in
       proxyPass = "http://127.0.0.1:9000";
       proxyWebsockets = true;
     };
-    locations."/web" = {
+  };
+  services.nginx.virtualHosts."minio-console.int.chir.rs" = {
+    forceSSL = true;
+    http2 = true;
+    listenAddresses = listenIPs;
+    sslCertificate = "/var/lib/acme/int.chir.rs/cert.pem";
+    sslCertificateKey = "/var/lib/acme/int.chir.rs/key.pem";
+    locations."/" = {
       proxyPass = "http://127.0.0.1:9001";
       proxyWebsockets = true;
     };
