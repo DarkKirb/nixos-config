268389e00d31124ead96bd4aa69f3fa8d9da8ba3
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-17 21:07:20 +0100

    add api.chir.rs vhost

--- config/services/chir-rs.nix
+++ config/services/chir-rs.nix
@@ -1,12 +1,22 @@
-{ pkgs, ... } @ args: {
+{ pkgs, ... }: {
   systemd.services.chirrs = {
     enable = true;
-    description = builtins.trace args "chir.rs";
+    description = "chir.rs";
     script = "${pkgs.chir-rs}/chir-rs-server";
     serviceConfig = {
       WorkingDirectory = pkgs.chir-rs;
       EnvironmentFile = "/run/secrets/services/chir.rs";
     };
     wantedBy = [ "multi-user.target" ];
+  };
+  services.nginx.virtualHosts."api.chir.rs" = {
+    forceSSL = true;
+    http2 = true;
+    listenAddresses = [ "0.0.0.0" "[::]" ];
+    sslCertificate = "/var/lib/acme/chir.rs/cert.pem";
+    sslCertificateKey = "/var/lib/acme/chir.rs/key.pem";
+    locations."/" = {
+      proxyPass = "http://localhost:8621/api.chir.rs/";
+    };
   };
 }
