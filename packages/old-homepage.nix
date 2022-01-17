c6e45a16ec004bbd9aafd85ebc428a3c01a5b1b0
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-17 17:01:16 +0100

    rename old-homepage

--- packages/old-homepage.nix
+++ packages/old-homepage.nix
@@ -1,5 +1,5 @@
 { ... }: {
-  old-homepage = builtins.fetchTarball {
+  homepage-old = builtins.fetchTarball {
     url = "https://minio.int.chir.rs/darkkirb.de/homepage.tar.zst";
     sha256 = "0m449cm13hamxpsqn9p6f6fjf93iz96vp08ghwrbdf8png4n86bp";
   };
