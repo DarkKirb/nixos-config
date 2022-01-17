3f8698ba8b8a84a711092815ef7299468ad04b57
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-17 15:22:36 +0100

    Revert "Add impure minio secret"
    This reverts commit a5845170a31c8070858877323bc1a00b7719fdfe.

--- config/sops.nix
+++ config/sops.nix
@@ -7,5 +7,4 @@
   sops.secrets."security/restic/password" = { };
   sops.secrets."security/minio/credentials_file" = { };
   sops.secrets."services/gitea.nix" = { };
-  sops.secrets."services/minio.nix" = { };
 }
