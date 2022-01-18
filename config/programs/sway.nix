6d187b4714c4e7d79f638850dbdc486254ae094b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-18 14:20:29 +0100

    set modifier to mod4 and fix ctrls_toggle

--- config/programs/sway.nix
+++ config/programs/sway.nix
@@ -2,11 +2,12 @@
   wayland.windowManager.sway = {
     enable = true;
     config = {
+      modifier = "Mod4";
       input = {
         "*" = {
           xkb_layout = "de,de";
           xkb_variant = "neo_qwertz,neo";
-          xkb_options = "ctrls_toggle";
+          xkb_options = "grp:ctrls_toggle";
         };
       };
     };
