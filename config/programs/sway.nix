0259e93b48f0661ccd8dbace2dd6848eed9709f3
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-18 14:02:20 +0000

    Add monitor configuration

--- config/programs/sway.nix
+++ config/programs/sway.nix
@@ -10,6 +10,19 @@
           xkb_options = "grp:ctrls_toggle";
         };
       };
+      outputs = {
+        "DP-1" = {
+          mode = "2560x1440@74.971Hz";
+          position = "0 0";
+          subpixel = "rgb";
+          adaptive_sync = "on";
+        };
+        "HDMI-A-1" = {
+          mode = "1920x1080@60Hz";
+          position = "2560 0";
+          subpixel = "rgb";
+        };
+      };
     };
   };
 }
