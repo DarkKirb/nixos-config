08401509ffc5916d14291c170eb05b8f62b28947
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-18 14:15:23 +0000

    use breeze dark

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -1,4 +1,6 @@
 { pkgs, ... }: {
   gtk.iconTheme.package = pkgs.numix-icon-theme;
   gtk.iconTheme.name = "Numix Dark";
+  gtk.theme.package = pkgs.libsForQt5.breeze-gtk;
+  gtk.theme.name = "Breeze Dark";
 }
