9b50841faf8d872d5728038d3c9869cf710c28b7
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-18 14:33:10 +0100

    Add pgp

--- config/home-manager/darkkirb.nix
+++ config/home-manager/darkkirb.nix
@@ -1,5 +1,6 @@
 desktop: { pkgs, ... }: {
   imports = [
     ./base.nix
+    ../programs/gpg.nix
   ] ++ (if desktop then [ ../programs/sway.nix ../programs/firefox.nix ] else []);
 }
---
+++ config/programs/gpg.nix
@@ -0,0 +1,17 @@
+{ ... }: {
+  programs.gpg = {
+    enable = true;
+    mutableKeys = false;
+    mutableTrust = false;
+    publicKeys = [
+      {
+        source = ../../keys/darkkirb_darkkirb.de.pgp
+        trust = 5;
+      }
+      {
+        source = ../../keys/mdelenk_hs-mittweida.de.pgp;
+        trust = 5;
+      }
+    ];
+  };
+}
