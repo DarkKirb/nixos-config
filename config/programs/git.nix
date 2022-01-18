f9190e8a1cfc5838a3239ad20f75f505a0935511
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-18 14:50:28 +0100

    set key:w

--- config/programs/git.nix
+++ config/programs/git.nix
@@ -4,6 +4,7 @@
     package = pkgs.gitAndTools.gitFull;
     lfs.enable = true;
     signing.signByDefault = true;
+    signing.key = "206DA5E1DA0904B6EE4916BA3CEF5DDA915AECB0";
     userEmail = "lotte@chir.rs";
     userName = "Charlotte 🦝 Delenk";
   };
