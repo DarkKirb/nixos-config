8b873819637a5509a478009c4763d82e2d250f96
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-19 15:09:42 +0100

    add sops secrets for email passwords

--- config/users/darkkirb.nix
+++ config/users/darkkirb.nix
@@ -10,4 +10,7 @@
     isNormalUser = true;
     uid = 1000;
   };
+  sops.secrets."email/darkkirb@darkkirb.de" = { owner = "darkkirb"; };
+  sops.secrets."email/lotte@chir.rs" = { owner = "darkkirb"; };
+  sops.secrets."email/mdelenk@hs-mittweida.de" = { owner = "darkkirb"; };
 }
