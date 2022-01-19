090c9b8e066a5acc2ac8e5a42e289aeae538c84b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-19 09:59:45 +0100

    put the enable in the correct scope

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -7,39 +7,40 @@
 
   # Paraiso (dark) by Chris Kempson
   # Alacritty colors
-  programs.alacritty.settings = {
+  programs.alacritty = {
     enable = true;
-    colors = {
-      # Default Colors
-      primary = {
-        background = "0x2f1e2e";
-        foreground = "0xa39e9b";
-      };
+    settings = {
+      colors = {
+        # Default Colors
+        primary = {
+          background = "0x2f1e2e";
+          foreground = "0xa39e9b";
+        };
 
-      # Normal Colors
-      normal = {
-        black = "0x2f1e2e";
-        red = "0xef6155";
-        green = "0x48b685";
-        yellow = "0xfec418";
-        blue = "0x06b6ef";
-        magenta = "0x815ba4";
-        cyan = "0x5bc4bf";
-        white = "0xa39e9b";
-      };
+        # Normal Colors
+        normal = {
+          black = "0x2f1e2e";
+          red = "0xef6155";
+          green = "0x48b685";
+          yellow = "0xfec418";
+          blue = "0x06b6ef";
+          magenta = "0x815ba4";
+          cyan = "0x5bc4bf";
+          white = "0xa39e9b";
+        };
 
-      # Bright Colors
-      bright = {
-        black = "0x776e71";
-        red = "0xef6155";
-        green = "0x48b685";
-        yellow = "0xfec418";
-        blue = "0x06b6ef";
-        magenta = "0x815ba4";
-        cyan = "0x5bc4bf";
-        white = "0xe7e9db";
+        # Bright Colors
+        bright = {
+          black = "0x776e71";
+          red = "0xef6155";
+          green = "0x48b685";
+          yellow = "0xfec418";
+          blue = "0x06b6ef";
+          magenta = "0x815ba4";
+          cyan = "0x5bc4bf";
+          white = "0xe7e9db";
+        };
       };
     };
   };
-
 }
