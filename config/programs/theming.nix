681f6e6870823383fb9c138f79ddd6366b02e61b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-19 09:54:36 +0100

    Add a purple theme to alacritty

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -4,4 +4,41 @@
   gtk.iconTheme.name = "Numix Dark";
   gtk.theme.package = pkgs.libsForQt5.breeze-gtk;
   gtk.theme.name = "Breeze Dark";
+
+  # Paraiso (dark) by Chris Kempson
+  # Alacritty colors
+  programs.alacritty.settings = {
+    colors = {
+      # Default Colors
+      primary = {
+        background = "0x2f1e2e";
+        foreground = "0xa39e9b";
+      };
+
+      # Normal Colors
+      normal = {
+        black = "0x2f1e2e";
+        red = "0xef6155";
+        green = "0x48b685";
+        yellow = "0xfec418";
+        blue = "0x06b6ef";
+        magenta = "0x815ba4";
+        cyan = "0x5bc4bf";
+        white = "0xa39e9b";
+      };
+
+      # Bright Colors
+      bright = {
+        black = "0x776e71";
+        red = "0xef6155";
+        green = "0x48b685";
+        yellow = "0xfec418";
+        blue = "0x06b6ef";
+        magenta = "0x815ba4";
+        cyan = "0x5bc4bf";
+        white = "0xe7e9db";
+      };
+    };
+  };
+
 }
