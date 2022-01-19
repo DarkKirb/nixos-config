69781ac9de40c630f1ee593b324f2c7ce1d3dbea
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-19 09:18:19 +0100

    escape single quotes

--- config/programs/vim.nix
+++ config/programs/vim.nix
@@ -155,7 +155,7 @@
       " Add (Neo)Vim's native statusline support.
       " NOTE: Please see `:h coc-status` for integrations with external plugins that
       " provide custom statusline: lightline.vim, vim-airline.
-      set statusline^=%{coc#status()}%{get(b:,'coc_current_function','')}
+      set statusline^=%{coc#status()}%{get(b:,'coc_current_function',\'\')}
 
       " Mappings for CoCList
       " Show all diagnostics.
