23ded5010a8406515e9ad4132bbd56a032decde5
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-19 20:26:26 +0100

    Add persistent keepalive to static wg0 peers

--- config/wireguard.nix
+++ config/wireguard.nix
@@ -12,6 +12,7 @@
             "fd0d:a262:1fa6:e621:b4e1:08ff:e658:6f49/64"
           ];
           endpoint = "138.201.155.128:51820";
+          persistentKeepalive = 25;
         }
         {
           publicKey = "YYQmSJwipRkZJUsPV5DxhfyRBMdj/O1XzN+cGYtUi1s=";
@@ -28,6 +29,7 @@
             "fd00:e621:e621:1::/64"
             "fd00:e621:e621:2::3/128"
           ];
+          persistentKeepalive = 25;
         }
         # Old infra: nas
         {
