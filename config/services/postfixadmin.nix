b5dc37d0a643f4dd315ee73776cfd6ee1b7c66bb
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-20 10:18:07 +0100

    fix group name

--- config/services/postfixadmin.nix
+++ config/services/postfixadmin.nix
@@ -33,5 +33,5 @@
     sslCertificate = "/var/lib/acme/chir.rs/cert.pem";
     sslCertificateKey = "/var/lib/acme/chir.rs/key.pem";
   };
-  services.phpfpm.pools.postfixadmin.group = "acme"; # there is no nginx group
+  services.phpfpm.pools.postfixadmin.settings."listen.group" = "acme"; # there is no nginx group
 }
