869b3c9c0bc1ad38f51f5468a14891e9b0343d6b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-20 09:57:32 +0100

    add postfixadmin

---
+++ config/services/postfixadmin.nix
@@ -0,0 +1,36 @@
+{ ... }: {
+  services.postfixadmin = {
+    enable = true;
+    adminEmail = "lotte@chir.rs";
+    database = {
+      dbname = "postfix";
+      host = "localhost";
+      passwordFile = "/run/secrets/services/postfixadmin/dbpassword";
+      username = "postfixadmin";
+    };
+    hostName = "mail.chir.rs";
+    setupPasswordFile = "/run/secrets/services/postfixadmin/setupPassword";
+  };
+  sops.secrets."services/postfixadmin/dbpassword" = {
+    owner = "nginx";
+  };
+  sops.secrets."services/postfixadmin/setupPassword" = {
+    owner = "nginx";
+  };
+  services.postgresql.ensureDatabases = [ "postfix" ];
+  services.postgresql.ensureUsers = [
+    {
+      name = "postfixadmin";
+      ensurePermissions = {
+        "DATABASE \"postfix\"" = "ALL PRIVILEGES";
+      };
+    }
+  ];
+  services.nginx.virtualHosts."mail.chir.rs" = {
+    forceSSL = true;
+    http2 = true;
+    listenAddresses = [ "0.0.0.0" "[::]" ];
+    sslCertificate = "/var/lib/acme/chir.rs/cert.pem";
+    sslCertificateKey = "/var/lib/acme/chir.rs/key.pem";
+  };
+}
