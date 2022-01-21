a93393cd9cf6f94b2486a01b34b7db1cef6bffd6
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-21 18:03:35 +0100

    exclude caches from the backup

--- config/services/restic.nix
+++ config/services/restic.nix
@@ -5,6 +5,13 @@
       "/var"
       "/home"
     ];
+    extraBackupArgs = [
+      "--exclude-caches"
+      "--exclude=/var/lib/minio/disk0/cache.int.chir.rs" # Cache files, don’t need backups since they are automatically deleted anyways…
+      "--exclude=/var/lib/minio/disk1/cache.int.chir.rs"
+      "--exclude=/var/lib/minio/disk2/cache.int.chir.rs"
+      "--exclude=/var/lib/minio/disk3/cache.int.chir.rs"
+    ];
     repository = "sftp:darkkirb@backup.int.chir.rs:/backup";
   };
   sops.secrets."security/restic/password" = { };
