0d2b4f01acd504a804752ddd31eb55b5b44cae09
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-01-30 14:05:27 +0100

    Add build script

---
+++ build-all.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+nix flake update
+nixos-rebuild build --flake '.#nutty-noon' -j16
+nixos-rebuild build --flake '.#nixos-8gb-fsn1-1' -j16
+nixos-rebuild build --flake '.#thinkrac' -j16
+
+rm -rf result
+
+nix copy --to 's3://cache.int.chir.rs?scheme=http&endpoint=10.0.2.2:9000' --all
