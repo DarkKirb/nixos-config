303446a1bd530e5ca44b7848f1329ddf22fda28d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-09 08:19:39 +0100

    Update dns.nix

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@ rec {
   inputs.nix-gaming.inputs.nixpkgs.follows = "nixpkgs";
   inputs.polymc.url = "github:PolyMC/PolyMC";
   inputs.polymc.inputs.nixpkgs.follows = "nixpkgs";
-  inputs.dns.url = "github:DarkKirb/dns.nix";
+  inputs.dns.url = "github:DarkKirb/dns.nix?ref=master";
   inputs.dns.inputs.nixpkgs.follows = "nixpkgs";
 
   outputs = { self, nixpkgs, sops-nix, home-manager, chir-rs, nur, nix-gaming, polymc, ... } @ args: {
