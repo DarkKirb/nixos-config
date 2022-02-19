6a906d0fb99ffcd51d58a6f954d649d1a1e54261
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-19 15:37:50 +0100

    Add whitelist entries

--- config/services/minecraft.nix
+++ config/services/minecraft.nix
@@ -5,5 +5,15 @@
 
   services.minecraft = {
     enable = true;
+    whitelist = [
+      {
+        uuid = "45821aee-c1f4-4a47-af23-6e2983f37ce6";
+        name = "DragonKing337";
+      }
+      {
+        uuid = "a6578f9a-288d-44af-8f43-e6402b126bb6";
+        name = "DarkKirb";
+      }
+    ];
   };
 }
