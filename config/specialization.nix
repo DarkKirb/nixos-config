db557ff134902cab8e7d65d9ba0c6e9eeefe4f86
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-19 08:03:48 +0100

    super -> prev

--- config/specialization.nix
+++ config/specialization.nix
@@ -2,9 +2,9 @@
 { config, ... }: {
   nixpkgs.overlays = [
     (self: prev: {
-      linuxKernel.kernels.linux_xanmod = super.linuxKernel.manualConfig {
-        inherit (super) stdenv hostPlatform;
-        inherit (super.linuxKernel.kernels.linux_xanmod) src version;
+      linuxKernel.kernels.linux_xanmod = prev.linuxKernel.manualConfig {
+        inherit (prev) stdenv hostPlatform;
+        inherit (prev.linuxKernel.kernels.linux_xanmod) src version;
         configfile = ../extra/linux/config-${config.networking.hostName};
       };
     })
