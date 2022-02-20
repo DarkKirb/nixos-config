40ad3f80cfd11d9b4bb233096eadaf72f53fec9c
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-20 21:57:49 +0100

    add essentialsx-chat and essentialsx-spawn

--- modules/minecraft/essentialsx.nix
+++ modules/minecraft/essentialsx.nix
@@ -12,15 +12,42 @@ in
       description = "Enable EssentialsX";
       type = types.bool;
     };
+    enableChat = mkOption {
+      default = true;
+      description = "Enable EssentialsX Chat";
+      type = types.bool;
+    };
+    enableSpawn = mkOption {
+      default = true;
+      description = "Enable EssentialsX Spawn";
+      type = types.bool;
+    };
   };
 
-  config = mkIf cfg.enable {
-    services.minecraft = {
-      vault.enable = config.services.minecraft.luckperms.enable;
+  config = mkIf cfg.enable
+    {
+      services.minecraft = {
+        vault.enable = config.services.minecraft.luckperms.enable;
+      };
+      services.minecraft.plugins = lib.mkMerge [
+        [{
+          package = essentialsx.essentialsx;
+          startScript = pkgs.writeScript "dummy" "";
+        }]
+        (
+          mkIf cfg.enableChat
+            [{
+              package = essentialsx.essentialsx-chat;
+              startScript = pkgs.writeScript "dummy" "";
+            }]
+        )
+        (
+          mkIf cfg.enableSpawn
+            [{
+              package = essentialsx.essentialsx-spawn;
+              startScript = pkgs.writeScript "dummy" "";
+            }]
+        )
+      ];
     };
-    services.minecraft.plugins = [{
-      package = essentialsx.essentialsx;
-      startScript = pkgs.writeScript "dummy" "";
-    }];
-  };
 }
