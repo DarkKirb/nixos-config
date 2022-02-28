6f1d2e38ec9be4ee8eca5b045bf965bb22604f7a
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-02-28 15:37:43 +0100

    update hydra

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -1,4 +1,23 @@
-{ nixpkgs-soundtouch, nixpkgs-tdesktop, system, ... }:
+{ nixpkgs-soundtouch, nixpkgs-tdesktop, system, pkgs, nixpkgs, ... }:
+let
+  rawPerlPackages = pkgs.callPackage "${nixpkgs}/pkgs/top-level/perl-packages.nix" {
+    overrides = pkgs: { };
+    buildPerl = pkgs.perl;
+  };
+  hydra = pkgs.callPackage "${nixpkgs}/pkgs/development/tools/misc/hydra/common.nix" {
+    version = "2022-02-22";
+    src = pkgs.fetchFromGitHub {
+      owner = "nixos";
+      repo = "hydra";
+      rev = "a2546121f0c737b9fa5e6d311561ce57e7d0318a";
+      sha256 = "092akmayvfwnqk17kl6jlzn5adi574xib9alw0w35rmavxp701zz";
+    };
+    nix = pkgs.nixVersions.unstable;
+    tests = {
+      basic = pkgs.nixosTests.hydra.hydra-unstable;
+    };
+  };
+in
 {
   nixpkgs.overlays = [
     (self: prev: {
@@ -7,7 +26,7 @@
       });
       soundtouch = nixpkgs-soundtouch.legacyPackages.${system}.soundtouch;
       tdesktop = nixpkgs-tdesktop.legacyPackages.${system}.tdesktop;
-      hydra-unstable = prev.hydra-unstable.overrideAttrs (old: {
+      hydra-unstable = hydra.overrideAttrs (old: {
         patches = [
           ../../extra/hydra.patch
         ];
