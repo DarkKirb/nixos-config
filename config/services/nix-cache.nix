3ea92074e802495f27bc7d4d52d9127f38a6e46c
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-02 21:36:11 +0100

    make the cache internal-only

--- config/services/nix-cache.nix
+++ config/services/nix-cache.nix
@@ -1,4 +1,11 @@
-{ ... }: {
+{ lib, config, pkgs, ... }:
+let
+  listenIPs = (import ../../utils/getInternalIP.nix config).listenIPs;
+  listenStatements = lib.concatStringsSep "\n" (builtins.map (ip: "listen ${ip}:443 http3;") listenIPs) + ''
+    add_header Alt-Svc 'h3=":443"';
+  '';
+in
+{
   imports = [
     (import ../../modules/gateway-st.nix {
       name = "nix-cache";
@@ -6,6 +13,7 @@
     })
   ];
   services.nginx.virtualHosts."cache.int.chir.rs" = {
+    listenAddresses = listenIPs;
     sslCertificate = "/var/lib/acme/int.chir.rs/cert.pem";
     sslCertificateKey = "/var/lib/acme/int.chir.rs/key.pem";
     locations."/" = {
