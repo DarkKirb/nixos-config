a1bcc25c83a1812be31763ce1b4d84f3a05f37d0
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-03 19:41:23 +0100

    enable multipart upload?

--- config/services/nix-cache-upload.nix
+++ config/services/nix-cache-upload.nix
@@ -8,7 +8,7 @@ in
     script = ''
       #!${pkgs.bash}/bin/bash
       ${pkgs.nix}/bin/nix store sign --key-file /root/cache-priv-key.pem --all
-      ${pkgs.nix}/bin/nix copy --to 's3://nix-cache?scheme=https&endpoint=cache.int.chir.rs' --all
+      ${pkgs.nix}/bin/nix copy --to 's3://nix-cache?scheme=https&endpoint=cache.int.chir.rs&multipart-upload=true' --all
       ${pkgs.minio-client}/bin/mc rm -r --older-than 7d nix-cache/nix-cache 
     '';
   };
