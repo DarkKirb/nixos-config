205f452250a60a291cbdca58393d7395d3ebcaff
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-03 10:16:05 +0100

    Sign and upload to the new cache

--- config/services/nix-cache-upload.nix
+++ config/services/nix-cache-upload.nix
@@ -7,12 +7,10 @@ in
     description = "Copy nix cache to cache.int.chir.rs";
     script = ''
       #!${pkgs.bash}/bin/bash
-      ${pkgs.nix}/bin/nix copy --to 's3://cache.int.chir.rs?scheme=http&endpoint=192.168.2.1:9000' --all
+      ${pkgs.nix}/bin/nix store sign --key-file /root/cache-priv-key.pem --all
+      ${pkgs.nix}/bin/nix copy --to 's3://nix-cache?scheme=https&endpoint=cache.int.chir.rs' --all
+      ${pkgs.minio-client}/bin/mc rm -r --older-than 7d nix-cache/nix-cache 
     '';
-    unitConfig = {
-      User = "darkkirb";
-      Group = "users";
-    };
   };
   systemd.timers.copy-to-cache = {
     enable = true;
