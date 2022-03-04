0d0d13b5ba230bad58ca1bd842acfc3d897606e8
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-04 10:54:04 +0100

    Add plover wayland patch

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -1,12 +1,12 @@
-{ nixpkgs-soundtouch, system, pkgs, nixpkgs, ... }:
+{ nixpkgs-soundtouch, system, pkgs, nixpkgs, ... }: with pkgs;
 let
-  rawPerlPackages = pkgs.callPackage "${nixpkgs}/pkgs/top-level/perl-packages.nix" {
+  rawPerlPackages = callPackage "${nixpkgs}/pkgs/top-level/perl-packages.nix" {
     overrides = pkgs: { };
-    buildPerl = pkgs.perl;
+    buildPerl = perl;
   };
-  hydra = pkgs.callPackage "${nixpkgs}/pkgs/development/tools/misc/hydra/common.nix" {
+  hydra = callPackage "${nixpkgs}/pkgs/development/tools/misc/hydra/common.nix" {
     version = "2021-08-11";
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "NixOS";
       repo = "hydra";
       rev = "9bce425c3304173548d8e822029644bb51d35263";
@@ -14,8 +14,25 @@ let
     };
     nix = (import nixpkgs-soundtouch { inherit system; }).nixVersions.unstable;
     tests = {
-      basic = pkgs.nixosTests.hydra.hydra-unstable;
+      basic = nixosTests.hydra.hydra-unstable;
     };
+  };
+  plover = with python3Packages; mkDerivationWith buildPythonPackages rec {
+    pname = "plover-wayland";
+    version = " 2022-01-14";
+    src = fetchFromGitHub {
+      owner = "matteodelabre";
+      repo = "plover";
+      rev = "fd5668a3ad9bd091289dd2e5e8e2c1dec063d51f";
+      sha256 = "0y3mdfqjv3vmv5c0cpvfa2mqdylan44iw1js480sxvklq8sxq6yv";
+    };
+    postPatch = "sed -i /PyQt5/d setup.cfg";
+    checkInputs = [ pytest mock ];
+    propagatedBuildInputs = [ Babel pyqt5 xlib pyserial appdirs wcwidth setuptools ];
+    dontWrapQtApps = true;
+    preFixup = ''
+      makeWrapperArgs+=("''${qtWrapperArgs[@]}")
+    '';
   };
 in
 {
@@ -30,6 +47,7 @@ in
           ../../extra/hydra.patch
         ];
       });
+      plover.dev = plover;
     })
   ];
 }
