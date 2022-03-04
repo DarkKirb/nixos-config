8b11aa29f05ac5a99624f3e1a1c622b51005142a
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-04 11:30:29 +0100

    fix plover build

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -17,6 +17,26 @@ let
       basic = nixosTests.hydra.hydra-unstable;
     };
   };
+  rtf-tokenize = with python3Packages; buildPythonPackage rec {
+    pname = "rtf_tokenize";
+    version = "1.0.0";
+    src = fetchFromGitHub {
+      owner = "benoit-pierre";
+      repo = pname;
+      rev = version;
+      sha256 = "1l5pfrggil9knk58r2r84i9msm7mdhddl87hkfk54qqk2sqzc06g";
+    };
+  };
+  plover-stroke = with python3Packages; buildPythonPackage rec {
+    pname = "plover_stroke";
+    version = "1.0.1";
+    src = fetchFromGitHub {
+      owner = "benoit-pierre";
+      repo = pname;
+      rev = version;
+      sha256 = "15by14gn2grvn7835hcrijfmccy4bqwvbg38rn8fvgyl6n2zhwzn";
+    };
+  };
   plover = with python3Packages; libsForQt5.mkDerivationWith buildPythonPackage rec {
     pname = "plover-wayland";
     version = "2022-01-14";
@@ -26,13 +46,33 @@ let
       rev = "fd5668a3ad9bd091289dd2e5e8e2c1dec063d51f";
       sha256 = "0y3mdfqjv3vmv5c0cpvfa2mqdylan44iw1js480sxvklq8sxq6yv";
     };
-    postPatch = "sed -i /PyQt5/d setup.cfg";
+    postPatch = ''
+      sed -i /PyQt5/d setup.cfg
+      sed -i 's|/usr/share/wayland|${wayland}/share/wayland|' plover_build_utils/setup.py
+      sed -i 's|pywayland==|pywayland>=|' reqs/dist.txt
+    '';
     checkInputs = [ pytest mock ];
-    propagatedBuildInputs = [ Babel pyqt5 xlib pyserial appdirs wcwidth setuptools pywayland ];
+    propagatedBuildInputs = [
+      Babel
+      pyqt5
+      xlib
+      pyserial
+      appdirs
+      wcwidth
+      setuptools
+      pywayland
+      pkg-config
+      plover-stroke
+      rtf-tokenize
+    ];
     dontWrapQtApps = true;
+    preBuild = ''
+      export PKG_CONFIG="${pkg-config}/bin/pkg-config"
+    '';
     preFixup = ''
       makeWrapperArgs+=("''${qtWrapperArgs[@]}")
     '';
+    installCheckPhase = "true";
   };
 in
 {
