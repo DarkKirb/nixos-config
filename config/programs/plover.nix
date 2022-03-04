6b896c73a5bbd5418c733a2acf04c3820eab6418
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-04 20:20:54 +0100

    check in plover dictionaries

--- config/programs/plover.nix
+++ config/programs/plover.nix
@@ -1,5 +1,17 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+let
+  plover-src = pkgs.plover.dev.src;
+in
+{
   home.packages = [
     pkgs.plover.dev
   ];
+  home.file = {
+    ".config/plover/main.json".source = "${plover-src}/plover/assets/main.json";
+    ".config/plover/commands.json".source = "${plover-src}/plover/assets/commands.json";
+    ".config/plover/user.json".text = builtins.toJSON {
+      "SER/TKPWAL" = "Sergal";
+      "SERLG" = "Sergal";
+    };
+  };
 }
