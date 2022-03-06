3ec680e3a5c60a09ca4c1c77db3c9d5a6397a4c1
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-06 11:17:11 +0100

    feat: Add keepassxc as systemd service
    fix #31
    fix #26

--- config/programs/keepass.nix
+++ config/programs/keepass.nix
@@ -1,3 +1,15 @@
 { pkgs, ... }: {
   home.packages = [ pkgs.keepassxc ];
+  systemd.user.services.keepassxc = {
+    Unit = {
+      Description = "keepassxc";
+      PartOf = [ "graphical-session.target" ];
+    };
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+    Service = {
+      ExecStart = "${pkgs.keepassxc}/bin/keepassxc";
+    };
+  };
 }
