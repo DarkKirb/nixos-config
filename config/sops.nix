c2c8bfdde8cc0899203121d86f841b61be25322e
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-09 17:27:09 +0100

    feat: move the secrets into the parent directory
    fix #58

--- config/sops.nix
+++ config/sops.nix
@@ -1,5 +1,5 @@
 { config, ... }:
 {
   sops.age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-  sops.defaultSopsFile = ../secrets + "/${config.networking.hostName}/secrets.yaml";
+  sops.defaultSopsFile = ../secrets + "/${config.networking.hostName}.yaml";
 }
