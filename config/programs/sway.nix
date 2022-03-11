da62090a536dd845f2eb2174f4a8eea44f6a0561
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-11 20:55:04 +0100

    fix: Fix the name of the Microphone Mute key
    fix #61

--- config/programs/sway.nix
+++ config/programs/sway.nix
@@ -39,7 +39,7 @@
           "XF86AudioRaiseVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ +5%";
           "XF86AudioLowerVolume" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-volume @DEFAULT_SINK@ -5%";
           "XF86AudioMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-sink-mute @DEFAULT_SINK@ toggle";
-          "XF86MicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
+          "XF86AudioMicMute" = "exec ${pkgs.pulseaudio}/bin/pactl set-source-mute @DEFAULT_SOURCE@ toggle";
           "XF86MonBrightnessDown" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set 5%-";
           "XF86MonBrightnessUp" = "exec ${pkgs.brightnessctl}/bin/brightnessctl set +5%";
           "XF86AudioPlay" = "exec ${pkgs.mpc-cli}/bin/mpc toggle";
