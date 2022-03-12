60373d30429544290ed6504872269611d628c1f7
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-12 11:25:32 +0100

    fix: Allow the dns tkey for darkkirb.de
    fix #67

--- config/services/named.nix
+++ config/services/named.nix
@@ -33,6 +33,11 @@ in
         masters = [
           "fd00:e621:e621::1"
         ];
+        extraConfig = ''
+          update-policy {
+            grant certbot. name _acme-challenge.darkkirb.de. txt;
+          };
+        '';
         file = "darkkirb.de.zone";
       };
       "chir.rs" = {
