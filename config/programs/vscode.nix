e49cba6a964b20dd4278eb83326dbb830bf2d87c
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-20 18:50:38 +0100

    fix: Switch to regular vscode

--- config/programs/vscode.nix
+++ config/programs/vscode.nix
@@ -1,7 +1,6 @@
 { pkgs, ... }: {
   programs.vscode = {
     enable = true;
-    package = pkgs.vscodium;
     extensions = (with pkgs.vscode-extensions; [
       arrterian.nix-env-selector
       bbenoist.nix
