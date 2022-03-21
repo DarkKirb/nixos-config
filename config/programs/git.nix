9fbb79b4dde19d6401e22ff94380fc28d9269603
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-21 15:55:37 +0100

    Remove git-mit

--- config/programs/git.nix
+++ config/programs/git.nix
@@ -1,24 +1,4 @@
 { pkgs, ... }:
-let gitTemplate = pkgs.stdenv.mkDerivation {
-  name = "git-template";
-  src = pkgs.writeTextDir "dummy/dummy" "";
-  nativeBuildInputs = with pkgs; with rust-binaries; [
-    git
-    mit-commit-msg
-    mit-pre-commit
-    mit-prepare-commit-msg
-  ];
-  buildPhase = "true";
-  installPhase = with pkgs; with rust-binaries; ''
-    git init $out --bare
-    cd $out
-    git branch -m main
-    ln -s ${mit-commit-msg}/bin/mit-commit-msg $out/hooks/commit-msg
-    ln -s ${mit-pre-commit}/bin/mit-pre-commit $out/hooks/pre-commit
-    ln -s ${mit-prepare-commit-msg}/bin/mit-prepare-commit-msg $out/hooks/prepare-commit-msg
-  '';
-};
-in
 {
   programs.git = {
     enable = true;
@@ -28,37 +8,5 @@ in
     signing.key = "AB2BD8DAF2E37122";
     userEmail = "lotte@chir.rs";
     userName = "Charlotte 🦝 Delenk";
-    extraConfig = {
-      init.templateDir = "${gitTemplate}";
-      "mit.lint" = {
-        body-wider-than-72-characters = true;
-        duplicated-trailers = true;
-        github-id-missing = true;
-        jira-issue-key-missing = false;
-        not-conventional-commit = true;
-        not-emoji-log = false;
-        pivotal-tracker-id-missing = false;
-        subject-line-ends-with-period = true;
-        subject-line-not-capitalized = false;
-        subject-longer-than-72-characters = true;
-        subject-not-separated-from-body = true;
-      };
-    };
   };
-  home.packages = with pkgs.rust-binaries; [
-    git-mit
-    git-mit-config
-    git-mit-install
-    git-mit-relates-to
-    mit-commit-message-lints
-    mit-commit-msg
-    mit-hook-test-helper
-    mit-pre-commit
-    mit-prepare-commit-msg
-  ];
-  xdg.configFile."git-mit/mit.toml".text = ''
-    [cd]
-    name = "Charlotte 🦝 Delenk"
-    email = "lotte@chir.rs"
-  '';
 }
