6efaed7c8f6cde5c631faceb2bf38313af4bfe96
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-03-31 19:12:58 +0100

    fix grapejuice config

--- config/games/grapejuice.nix
+++ config/games/grapejuice.nix
@@ -1,25 +1,9 @@
 { system, nix-gaming, ... } @ args: { lib, config, pkgs, ... }:
 let
   wine-tkg = nix-gaming.outputs.packages.${system}.wine-tkg;
-  hardware-profiles = {
-    nutty-noon = {
-      graphics_id = "a94037840edd11a1718c5e61863296c68fe0790392d2c0bc984ceb3d7236a90e";
-      gpu_vendor_id = 1;
-      gpu_pci_id = 1;
-      gpu_can_do_vulkan = true;
-      provider_index = 0;
-      provider_name = "Advanced Micro Devices, Inc. [AMD/ATI] Navi 14 ...";
-      should_prime = false;
-      use_mesa_gl_override = false;
-      preferred_roblox_renderer_string = "Vulkan";
-      is_multi_gpu = false;
-      version = 2;
-    };
-    thinkrac = { };
-  };
   grapejuice_config = {
     __version__ = 2;
-    __hardware_profiles__ = hardware-profiles.${args.config.networking.hostName};
+    __hardware_profiles__ = null;
     show_fast_flag_warning = true;
     no_daemon_mode = true;
     release_channel = "master";
@@ -28,7 +12,7 @@ let
     try_profiling_hardware = false;
     wineprefixes = [
       {
-        id = "4106ce6d-ab2d-4684-89cf-8ee0695e0150";
+        id = "ec33b6a3-8b44-4179-baec-54cb5bc888cb";
         priority = 0;
         name_on_disk = "player";
         display_name = "Player";
@@ -38,7 +22,7 @@ let
         use_mesa_gl_override = false;
         enable_winedebug = false;
         winedebug_string = "";
-        roblox_renderer = "Vulkan";
+        roblox_renderer = "OpenGL";
         env = { };
         hints = [
           "player"
@@ -51,35 +35,7 @@ let
         };
         third_party = {
           fps_unlocker = false;
-          dxvk = true;
-        };
-      }
-      {
-        id = "d5f4d5a5-d707-4821-b5b3-cdad9ff13d5b";
-        priority = 0;
-        name_on_disk = "studio";
-        display_name = "Studio";
-        wine_home = "${wine-tkg}";
-        dll_overrides = "dxdiagn=;winemenubuilder.exe=";
-        prime_offload_sink = -1;
-        use_mesa_gl_override = false;
-        enable_winedebug = false;
-        winedebug_string = "";
-        roblox_renderer = "Vulkan";
-        env = { };
-        hints = [
-          "studio"
-          "player"
-          "app"
-        ];
-        fast_flags = {
-          roblox_studio = { };
-          roblox_player = { };
-          roblox_app = { };
-        };
-        third_party = {
-          fps_unlocker = false;
-          dxvk = true;
+          dxvk = false;
         };
       }
     ];
