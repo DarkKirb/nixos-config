b8517653591404e9377d7402624e4a2b53d33c40
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-01 18:11:18 +0100

    fix username and uuid

--- config/services/minecraft.nix
+++ config/services/minecraft.nix
@@ -10,8 +10,8 @@
     enable = true;
     whitelist = [
       {
-        uuid = "45821aee-c1f4-4a47-af23-6e2983f37ce6";
-        name = "DragonKing337";
+        uuid = "74e2502d-64db-4ac4-bac7-a0a2bc50ec9f";
+        name = "DragonKing1337";
       }
       {
         uuid = "a6578f9a-288d-44af-8f43-e6402b126bb6";
