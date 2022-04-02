b23abdca848ab49a4a52e82ae57fb159f6d12c5d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-02 08:51:07 +0100

    add zfs services

--- config/nutty-noon.nix
+++ config/nutty-noon.nix
@@ -22,7 +22,11 @@
   ];
 
   boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.devNodes = true;
+  boot.zfs.devNodes = "/dev/";
+
+  services.zfs.trim.enable = true;
+  services.zfs.autoScrub.enable = true;
+  services.zfs.autoScrub.pools = ["ssd" "hdd"];
 
   boot.initrd.luks.devices = {
     ssd = {
