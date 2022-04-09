c5807200e01f4a9e61b62b9a57c596ecafb1c8eb
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-09 11:06:06 +0100

    fix theming?

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -3,9 +3,12 @@ let theme = import ../../extra/theme.nix; in
 {
   gtk.enable = true;
   gtk.iconTheme.package = pkgs.numix-icon-theme;
-  gtk.iconTheme.name = "Numix Dark";
+  gtk.iconTheme.name = "Numix";
   gtk.theme.package = pkgs.libsForQt5.breeze-gtk;
-  gtk.theme.name = "Breeze Dark";
+  gtk.theme.name = "Breeze-Dark";
+  qt.enable = true;
+  qt.style.package = pkgs.libsForQt5.breeze-qt5;
+  qt.style.name = "BreezeDark";
 
   # Paraiso (dark) by Chris Kempson
   # Alacritty colors
