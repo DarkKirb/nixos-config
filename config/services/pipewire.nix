61ce2cdffceef770e82f1b4f5ec8e39526e3713c
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-09 12:22:14 +0100

    add some rt stuff

--- config/services/pipewire.nix
+++ config/services/pipewire.nix
@@ -4,9 +4,18 @@
     pulse.enable = true;
     jack.enable = true;
     alsa.enable = true;
+    alsa.support32Bit = true;
   };
+  security.rtkit.enable = true;
+  hardware.pulseaudio.enable = false;
   xdg.portal = {
     enable = true;
     wlr.enable = true;
   };
+  security.pam.loginLimits = [
+    { domain = "@audio"; item = "memlock"; type = "-"; value = "unlimited"; }
+    { domain = "@audio"; item = "rtprio"; type = "-"; value = "99"; }
+    { domain = "@audio"; item = "nofile"; type = "soft"; value = "99999"; }
+    { domain = "@audio"; item = "nofile"; type = "hard"; value = "99999"; }
+  ];
 }
