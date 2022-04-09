942daa99332de6c79d6aecb58e5ae618775f7821
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-09 12:11:19 +0100

    make my server responsible for all of ipv6 private ip space

--- config/wireguard.nix
+++ config/wireguard.nix
@@ -10,7 +10,7 @@
         {
           publicKey = "zQY9cAzbRO/FgV92pda7yk0NJFSXzHfi6+tgRq3g/SY=";
           allowedIPs = [
-            "fd0d:a262:1fa6:e621:b4e1:08ff:e658:6f49/64"
+            "fd0d:a262:1fa6:e621:b4e1:08ff:e658:6f49/7"
           ];
           endpoint = "138.201.155.128:51820";
           persistentKeepalive = 25;
