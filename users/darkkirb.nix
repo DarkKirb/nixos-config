ce9955ecea3dea80c8e687731f02d2f90765c359
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-12 12:56:36 +0100

    Add the remainder of the options to darkkirb

--- users/darkkirb.nix
+++ users/darkkirb.nix
@@ -1,6 +1,17 @@
 { ... }: {
   users.users.darkkirb = {
+    createHome = true;
     description = "Charlotte 🦝 Delenk";
+    extraGroups = [
+      "wheel"
+    ];
+    group = "users";
+    home = "/home/darkkirb";
+    isNormalUser = true;
+    uid = 1000;
+    openssh.authorizedKeys.keys = [
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDXQlfvRUm/z6eP1EjsajIbMibkq9n+ymlbBi7NFiOuaAAAABHNzaDo= ssh:"
+    ];
     passwordFile = "/run/secrets-for-users/password/darkkirb";
   };
   sops.secrets."password/darkkirb" = {
