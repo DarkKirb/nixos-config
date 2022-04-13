ac549a0f5247a9ba4536ec74038b1a03a8067eaf
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-13 11:33:50 +0100

    fix ssh config

--- config/programs/ssh.nix
+++ config/programs/ssh.nix
@@ -1,19 +1,21 @@
 { ... }: {
   programs.ssh = {
     enable = true;
-    "build-nas" = {
-      hostname = "backup.int.chir.rs";
-      identitiesOnly = true;
-      identityFile = "~/.ssh/id_ed25519";
-      port = 22;
-      user = "darkkirb";
-    };
-    "build-pc" = {
-      hostname = "nutty-noon.int.chir.rs";
-      identitiesOnly = true;
-      identityFile = "~/.ssh/id_ed25519";
-      port = 22;
-      user = "darkkirb";
+    matchBlocks = {
+      "build-nas" = {
+        hostname = "backup.int.chir.rs";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519";
+        port = 22;
+        user = "darkkirb";
+      };
+      "build-pc" = {
+        hostname = "nutty-noon.int.chir.rs";
+        identitiesOnly = true;
+        identityFile = "~/.ssh/id_ed25519";
+        port = 22;
+        user = "darkkirb";
+      };
     };
   };
 }
