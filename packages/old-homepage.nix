b55385699c9b4175e0289397800dd560dc234346
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-16 11:06:48 +0100

    use publically available URL for homepage.tar.zst

--- packages/old-homepage.nix
+++ packages/old-homepage.nix
@@ -1,6 +1,6 @@
 { ... }: {
   homepage-old = builtins.fetchTarball {
-    url = "https://minio.int.chir.rs/static.darkkirb.de/homepage.tar.zst";
+    url = "https://static.darkkirb.de/homepage.tar.zst";
     sha256 = "1wf90kpb0ra0fy0msh1drmr4jjxw7c1q3ksqc9zfy04sjh6y5msg";
   };
 }
