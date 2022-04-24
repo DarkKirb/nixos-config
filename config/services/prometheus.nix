6e95290303143b936db971f89cb98bf1ddf5293c
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-24 16:10:00 +0100

    Track node_exporter for the nas

--- config/services/prometheus.nix
+++ config/services/prometheus.nix
@@ -14,6 +14,7 @@
             "[fd0d:a262:1fa6:e621:b4e1:8ff:e658:6f49]:${toString config.services.prometheus.exporters.node.port}"
             "[fd0d:a262:1fa6:e621:47e6:24d4:2acb:9437]:${toString config.services.prometheus.exporters.node.port}"
             "[fd0d:a262:1fa6:e621:f45a:db9f:eb7c:1a3f]:${toString config.services.prometheus.exporters.node.port}"
+            "[fd0d:a262:1fa6:e621:bc9b:6a33:86e4:873b]:${toString config.services.prometheus.exporters.node.port}"
           ];
         }];
       }
