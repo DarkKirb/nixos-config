3562b0797064e1c52da85da965533b741c04fd93
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-04-26 07:09:47 +0100

    please work?

--- config/services/reverse-proxy.nix
+++ config/services/reverse-proxy.nix
@@ -13,19 +13,12 @@
   services.nginx.virtualHosts."mastodon.chir.rs" = {
     sslCertificate = "/var/lib/acme/chir.rs/cert.pem";
     sslCertificateKey = "/var/lib/acme/chir.rs/key.pem";
-    root = "${config.services.mastodon.package}/public/"; # for spood
     locations."/" = {
-      tryFiles = "$uri @proxy";
-    };
-    locations."@proxy" = {
       proxyPass = "https://nas.int.chir.rs";
       proxyWebsockets = true;
       extraConfig = ''
         proxy_ssl_server_name on;
       '';
     };
-    extraConfig = ''
-      proxy_set_header Host "mastodon.chir.rs";
-    '';
   };
 }
