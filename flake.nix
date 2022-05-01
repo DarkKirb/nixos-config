5bca1d2b5632136d141e932057ec65056396f2a6
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-01 20:11:01 +0100

    just use normal hydra the nix-ca one barely works

--- flake.nix
+++ flake.nix
@@ -14,8 +14,8 @@ rec {
     polymc.url = "github:PolyMC/PolyMC";
     polymc.inputs.nixpkgs.follows = "nixpkgs";
     dns.url = "github:DarkKirb/dns.nix?ref=master";
-    hydra.url = github:thufschmitt/hydra/nix-ca;
-    nix.url = github:NixOS/nix/2.6.1;
+    hydra.url = github:NixOS/hydra;
+    nix.url = github:NixOS/nix/2.7.0;
     hydra.inputs.nix.follows = "nix";
     hydra.inputs.nixpkgs.follows = "nix/nixpkgs";
     hosts-list.url = github:StevenBlack/hosts;
