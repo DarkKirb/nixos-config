085845da13d6618eb3b60aebadbc876099bd4dc3
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-01 21:52:05 +0100

    lock to specific nix revision

--- flake.nix
+++ flake.nix
@@ -15,9 +15,9 @@ rec {
     polymc.inputs.nixpkgs.follows = "nixpkgs";
     dns.url = "github:DarkKirb/dns.nix?ref=master";
     hydra.url = github:NixOS/hydra;
-    #nix.url = github:NixOS/nix/2.7.0;
-    #hydra.inputs.nix.follows = "nix";
-    #hydra.inputs.nixpkgs.follows = "nix/nixpkgs";
+    nix.url = github:NixOS/nix/5fe4fe823c193cbb7bfa05a468de91eeab09058d;
+    hydra.inputs.nix.follows = "nix";
+    hydra.inputs.nixpkgs.follows = "nix/nixpkgs";
     hosts-list.url = github:StevenBlack/hosts;
     hosts-list.flake = false;
     nixos-hardware.url = github:NixOS/nixos-hardware;
