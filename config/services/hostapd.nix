8418449d26bd019f0665fffdbca1fb7acd0a6bbe
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-07 15:44:57 +0100

    i take it back lol

--- config/services/hostapd.nix
+++ config/services/hostapd.nix
@@ -6,12 +6,14 @@
     enable = true;
     countryCode = "DE";
     interface = "wlp6s0";
-    ssid = "racc";
+    ssid = "🦝";
     wpa = true;
     wpaPassphraseFile = config.sops.secrets."services/hostapd".path;
     extraConfig = ''
+      utf8_ssid=1
       wmm_enabled=1
       ieee80211n=1
+      wpa_pairwise=GCMP CCMP
     '';
   };
   sops.secrets."services/hostapd" = {
