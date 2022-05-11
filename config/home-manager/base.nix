2a1d1b1096315ad1242ca399fe75740922c9b707
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-11 15:57:10 +0100

    Disable zsh-nix-shell

--- config/home-manager/base.nix
+++ config/home-manager/base.nix
@@ -28,16 +28,6 @@ desktop: { pkgs, ... }: {
         XMODIFIERS = "@im=ibus";
       };
       plugins = [
-        {
-          name = "zsh-nix-shell";
-          file = "nix-shell.plugin.zsh";
-          src = pkgs.fetchFromGitHub {
-            owner = "chisui";
-            repo = "zsh-nix-shell";
-            rev = "v0.4.0";
-            sha256 = "037wz9fqmx0ngcwl9az55fgkipb745rymznxnssr3rx9irb6apzg";
-          };
-        }
       ];
     };
   };
