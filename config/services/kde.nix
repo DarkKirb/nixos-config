a7cfe2f86331a568ca4a2be3da8daa594d36dc29
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-11 12:54:17 +0100

    re-enable plasma

--- config/services/kde.nix
+++ config/services/kde.nix
@@ -1,3 +1,10 @@
 { ... }: {
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver = {
+    enable = true;
+    desktopManager.plasma.enable = true;
+    displayManager.sddm.enable = true;
+    libinput.enable = true;
+    layout = "de";
+    xkbVariant = "neo";
+  };
 }
