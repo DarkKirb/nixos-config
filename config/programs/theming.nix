46e878db759519ea7d6ced32373df7cdc91c857f
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-11 19:54:49 +0100

    reformat gtk config

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -1,11 +1,27 @@
 { pkgs, ... }:
 let theme = import ../../extra/theme.nix; in
 {
-  gtk.enable = true;
-  gtk.iconTheme.package = pkgs.numix-icon-theme;
-  gtk.iconTheme.name = "Numix";
-  gtk.theme.package = pkgs.libsForQt5.breeze-gtk;
-  gtk.theme.name = "Breeze-Dark";
+  gtk = {
+    enable = true;
+    cursorTheme = {
+      package = null;
+      name = "breeze_cursors";
+      size = 24;
+    };
+    font = {
+      package = null;
+      name = "Noto Sans";
+      size = 10;
+    };
+    iconTheme = {
+      package = null;
+      name = "breeze-dark";
+    };
+    theme = {
+      package = null;
+      name = "Breeze-Dark";
+    };
+  };
   qt.enable = true;
   qt.style.package = pkgs.libsForQt5.breeze-qt5;
   qt.style.name = "BreezeDark";
