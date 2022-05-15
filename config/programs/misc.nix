cee818eeea80a28df1baa8415470425e59eea41d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-15 07:18:54 +0100

    coc-nvim seems to not work

--- config/programs/misc.nix
+++ config/programs/misc.nix
@@ -5,5 +5,19 @@
     thunderbird
   ];
   xdg.configFile."gdb/gdbinit".text = "set auto-load safe-path /nix/store";
-
+  xsettingsd = {
+    enable = true;
+    settings = {
+      "Gtk/EnableAnimations" = 1;
+      "Gtk/DecorationLayout" = "icon:minimize,maximize,close";
+      "Gtk/PrimaryButtonWarpsSlider" = 0;
+      "Gtk/ToolbarStyle" = 3;
+      "Gtk/MenuImages" = 1;
+      "Gtk/ButtonImages" = 1;
+      "Gtk/CursorThemeSize" = 24;
+      "Gtk/CursorThemeName" = "breeze_cursors";
+      "Gtk/FontName" = "Noto Sans,  10";
+      "Net/IconThemeName" = "breeze-dark";
+    };
+  };
 }
