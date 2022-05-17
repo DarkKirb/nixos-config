52fa1cd2eaeed15dc3bae1ea336a8a5979100248
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-17 14:42:04 +0100

    use ibus-engines.table instead of ibus-table

--- packages/ibus-tp.nix
+++ packages/ibus-tp.nix
@@ -1,4 +1,4 @@
-{ stdenvNoCC, fetchFromGitHub, ibus, ibus-table }: stdenvNoCC.mkDerivation rec {
+{ stdenvNoCC, fetchFromGitHub, ibus, ibus-engines }: stdenvNoCC.mkDerivation rec {
   pname = "ibus-toki-pona";
   version = "20220215";
   src = fetchFromGitHub {
@@ -7,7 +7,7 @@
     rev = "43da00449a99c0b8aaa6f5099d0dc1f795c7c39f";
     sha256 = "sha256-CIa0wJnv1G0jpS8l2cjEFey1pdQuJPftiwZ5MZyriJ8=";
   };
-  buildInputs = [ ibus ibus-table ];
+  buildInputs = [ ibus ibus-engines.table ];
 
   buildPhase = ''
     ibus-table-createdb -n tokipona.db tokipona.txt
