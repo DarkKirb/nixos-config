052bde2a560f2525759ebecaa77edc17e42114df
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-05-24 10:23:40 +0100

    add fail2ban rules for dovecot and postfix

--- config/services/fail2ban.nix
+++ config/services/fail2ban.nix
@@ -3,5 +3,17 @@
     enable = true;
     bantime-increment.enable = true;
     bantime-increment.maxtime = "48h";
+    jails = {
+      dovecot = ''
+        enabled = true
+        filter   = dovecot
+        action   = iptables-multiport[name=dovecot, port="pop3,pop3s,imap,imaps", protocol=tcp]
+      '';
+      postfix = ''
+        enabled = true
+        filter   = postfix
+        action   = iptables-multiport[name=postfix, port="imap,imaps,submission", protocol=tcp]
+      '';
+    };
   };
 }
