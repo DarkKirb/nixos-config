50f6b4d95e7dbdd040df0b8cb966d384b09680ea
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-12 20:58:31 +0100

    fix base.lua

--- config/programs/vim/base.lua
+++ config/programs/vim/base.lua
@@ -3,7 +3,7 @@ local wo = vim.wo
 local bo = vim.bo
 local map = vim.api.nvim_set_keymap
 
-o.number = "ar"
+o.mouse = "ar"
 o.clipboard = "unnamedplus" -- The correct default clipboard
 o.cmdheight = 2 -- more space for displaying messages
 -- Having longer updatetime (default is 4000ms = 4s) leads to noticeable delays and poor user experience
@@ -11,7 +11,8 @@ o.updatetime = 300
 -- don’t pass messages to |ins-completion-menu|
 o.shortmess = o.shortmess .. "c"
 
-wo.number = "relativenumber"
+wo.number = true
+wo.relativenumber = true
 wo.signcolumn = "number"
 
 bo.tabstop = 2
