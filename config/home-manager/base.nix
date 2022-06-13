42af50d07ab49280286db24af0519dcfd2c682d9
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-13 16:21:32 +0100

    make the session variables systemd user session variables

--- config/home-manager/base.nix
+++ config/home-manager/base.nix
@@ -21,14 +21,15 @@ desktop: {pkgs, ...}: {
         kitty-integration
         unfunction kitty-integration
       '';
-      sessionVariables = {
-        GTK_IM_MODULE = "ibus";
-        QT_IM_MODULE = "ibus";
-        XMODIFIERS = "@im=ibus";
-      };
       plugins = [
       ];
     };
+  };
+  systemd.user.sessionVariables = {
+    GTK_IM_MODULE = "ibus";
+    QT_IM_MODULE = "ibus";
+    XMODIFIERS = "@im=ibus";
+    GLFW_IM_MODULE = "ibus";
   };
   home.file.".p10k.zsh".source = ./.p10k.zsh;
 
