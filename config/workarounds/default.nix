1aec11b8f8a377f9ea16c7131b7edc11929ef204
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-14 08:47:01 +0100

    patch kitty to work with the wide width of sitelen pona

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -113,6 +113,11 @@ in {
        */
       inherit (noto-variable) noto-fonts-cjk;
       inherit (go116) buildGo116Module;
+      kitty = prev.kitty.overrideAttrs (old: {
+        patches = [
+          ../../extra/kitty.patch
+        ];
+      });
     })
   ];
 }
