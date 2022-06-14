0091adced7eb20f6ce6d77315ccb9efc6e9899e0
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-14 14:28:34 +0100

    add fcitx5 table input

--- config/programs/fcitx.nix
+++ config/programs/fcitx.nix
@@ -3,6 +3,7 @@
     enabled = "fcitx5";
     fcitx5.addons = with pkgs; [
       fcitx5-mozc
+      fcitx5-chinese-addons # think this is required for table input?
       fcitx5-table-extra
       fcitx5-table-other
     ];
