2b6cdda7787573a068c2d8d7cd6cb3c7f41e2a4d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-19 06:46:18 +0100

    improve the update script

--- scripts/diff-drvs.py
+++ scripts/diff-drvs.py
@@ -16,34 +16,32 @@ packages = {}
 
 for drv in old_derivation:
     drvData = old_derivation[drv]
-    if "pname" in drvData["env"]:
-        version = drvData["env"]["version"] if "version" in drvData[
-            "env"] else drv
-        if drvData["env"]["pname"] in packages:
-            packages[drvData["env"]["pname"]]["old"].add(version)
-        else:
-            packages[drvData["env"]["pname"]] = {"old": {version}}
+    used_name = drvData["env"]["pname"] if "pname" in drvData["env"] else drvData["env"]["name"]
+    version = drvData["env"]["version"] if "version" in drvData["env"] else drv
+    if used_name in packages:
+        packages[used_name]["old"].add(version)
+    else:
+        packages[used_name] = {"old": {version}}
 
 for drv in new_derivation:
     drvData = new_derivation[drv]
-    if "pname" in drvData["env"]:
-        version = drvData["env"]["version"] if "version" in drvData[
-            "env"] else drv
-        if drvData["env"]["pname"] in packages:
-            if "new" in packages[drvData["env"]["pname"]]:
-                packages[drvData["env"]["pname"]]["new"].add(version)
-            else:
-                packages[drvData["env"]["pname"]]["new"] = {version}
+    used_name = drvData["env"]["pname"] if "pname" in drvData["env"] else drvData["env"]["name"]
+    version = drvData["env"]["version"] if "version" in drvData["env"] else drv
+    if used_name in packages:
+        if "new" in packages[used_name]:
+            packages[used_name]["new"].add(version)
         else:
-            packages[drvData["env"]["pname"]] = {"new": {version}}
+            packages[used_name]["new"] = {version}
+    else:
+        packages[used_name] = {"new": {version}}
 
 for package in packages:
     if "old" in packages[package] and "new" in packages[package]:
         if packages[package]["old"] != packages[package]["new"]:
             print(
-                f"{package}: {packages[package]['old']} -> {packages[package]['new']}"
+                f"{package}: {sorted(packages[package]['old'])} -> {sorted(packages[package]['new'])}"
             )
     elif "old" in packages[package]:
-        print(f"{package}: {packages[package]['old']} -> removed")
+        print(f"{package}: {sorted(packages[package]['old'])} -> removed")
     elif "new" in packages[package]:
-        print(f"{package}: added -> {packages[package]['new']}")
+        print(f"{package}: added -> {sorted(packages[package]['new'])}")
