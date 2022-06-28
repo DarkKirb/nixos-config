a86c8478aefa69db69d9712458aed864834936a5
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-28 21:11:55 +0100

    basic alot config

--- config/programs/mail.nix
+++ config/programs/mail.nix
@@ -5,6 +5,21 @@ in {
   programs.notmuch.enable = true;
   programs.alot = {
     enable = true;
+    hooks = builtins.readFile ./hooks.py
+    bindings = {
+      envelope = {
+        k = "call hooks.attach_my_key(ui)";
+        K = "call hooks.attach_keys(ui)";
+        "control k" = "call hooks.attach_recipient_keys(ui)";
+      };
+      thread = {
+        k = "call hooks.import_keys(ui)"
+      };
+    };
+    settings = {
+      envelope_txt2html = "${pkgs.pandoc}/bin/pandoc -f markdown -t html -s --self-contained --template=${../../extras/GitHub.html5}";
+      envelope_html2txt = "${pkgs.pandoc}/bin/pandoc -t markdown -f html"
+    };
   };
   programs.msmtp.enable = true;
 }
