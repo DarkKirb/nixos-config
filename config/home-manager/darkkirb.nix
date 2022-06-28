92a17c494b8154b922ae08f8f9fda976cbe73b7d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-06-28 19:40:55 +0100

    remove vscode

--- config/home-manager/darkkirb.nix
+++ config/home-manager/darkkirb.nix
@@ -27,7 +27,6 @@
         ../programs/keybase.nix
         ../programs/keepass.nix
         ../programs/kicad.nix
-        ../programs/vscode
         ../programs/misc.nix
         ../programs/mail.nix
         ../programs/kitty.nix
