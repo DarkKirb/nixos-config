16ccd0791b5db6c2f90c79d664eb75c1fdc010e6
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-01 08:39:16 +0100

    cleanup workaround

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -4,25 +4,12 @@
   nixpkgs,
   hydra,
   nixpkgs-noto-variable,
-  nixpkgs-go116,
   nix-packages,
   ...
 }:
 with pkgs; let
   hydra-pkg = hydra.defaultPackage.${system};
-  rtf-tokenize = with python3Packages;
-    buildPythonPackage rec {
-      pname = "rtf_tokenize";
-      version = "1.0.0";
-      src = fetchFromGitHub {
-        owner = "benoit-pierre";
-        repo = pname;
-        rev = version;
-        sha256 = "1l5pfrggil9knk58r2r84i9msm7mdhddl87hkfk54qqk2sqzc06g";
-      };
-    };
   noto-variable = import nixpkgs-noto-variable {inherit system;};
-  go116 = import nixpkgs-go116 {inherit system;};
 in {
   nixpkgs.overlays = [
     (self: prev: {
@@ -32,7 +19,6 @@ in {
           ../../extra/hydra.patch
         ];
       });
-      #plover.dev = plover;
       mosh = prev.mosh.overrideAttrs (old: {
         patches = [
           ./mosh/ssh_path.patch
@@ -53,15 +39,7 @@ in {
           sha256 = "09mvk9zxclkf4wrkkfzg0p2hx1f74gpymr0a0l3pckmk6za2n3d1";
         };
       });
-      /*
-       gtk3 = prev.gtk3.overrideAttrs (old: {
-       postPatch = old.postPatch + ''
-       sed -i 's/gtk_compose_table_save_cache (compose_table);//' gtk/gtkcomposetable.c
-       '';
-       });
-       */
       inherit (noto-variable) noto-fonts-cjk;
-      inherit (go116) buildGo116Module;
       inherit (nix-packages.packages.${system}) plover plover-plugins-manager regenpfeifer plover-regenpfeifer lotte-art;
       kitty = prev.kitty.overrideAttrs (old: {
         patches =
