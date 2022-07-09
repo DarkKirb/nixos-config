e74f50a25b5705bac302124c29de23e93a69100e
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-09 09:22:20 +0100

    change signing key id??

--- config/services/gitea.nix
+++ config/services/gitea.nix
@@ -52,7 +52,7 @@
       packages.ENABLED = true;
       federation.ENABLED = true;
       "repository.signing" = {
-        SIGNING_KEY = "823566455E49DDC6AE5813048CA13817A54AAB38";
+        SIGNING_KEY = "C9974EDF9932B558";
         SIGNING_NAME = "Gitea";
         SIGNING_EMAIL = "gitea@chir.rs";
         INITIAL_COMMIT = "always";
