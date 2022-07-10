b9011c72fdce3b8fa249eaee530d5519a091b30d
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-10 17:44:26 +0100

    make installer work

--- config/default.nix
+++ config/default.nix
@@ -1,1 +1,3 @@
-_: {}
+{ config, ... }: {
+  networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" config.networking.hostName);
+}
--- hosts/installer/default.nix
+++ hosts/installer/default.nix
@@ -1,1 +1,14 @@
-_: {}
+{
+  modulesPath,
+  ...
+}: {
+  imports = [
+    "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix"
+  ];
+  boot.supportedFilesystems = ["zfs"];
+  nixpkgs.localSystem = {
+    gcc.arch = "skylake";
+    gcc.tune = "skylake";
+    system = "x86_64-linux";
+  };
+}
