23e6e4f512f7c44914e2abbe63d82fbc76d1c2ba
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-12 15:14:53 +0100

    use systemd in initrd?

--- config/default.nix
+++ config/default.nix
@@ -5,6 +5,7 @@
 }: {
   networking.hostId = builtins.substring 0 8 (builtins.hashString "sha256" config.networking.hostName);
   networking.domain = "int.chir.rs";
+  boot.initrd.systemd.enable = true;
 
   imports = [
     ./nix.nix
