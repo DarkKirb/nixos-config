5e1b84f5e29ee198def8efe63d717a5bba519359
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-07-29 08:32:43 +0100

    please work now

--- config/workarounds/default.nix
+++ config/workarounds/default.nix
@@ -1,15 +1,14 @@
-{ system
-, pkgs
-, nixpkgs
-, nixpkgs-noto-variable
-, nix-packages
-, rnix-lsp
-, ...
+{
+  system,
+  pkgs,
+  nixpkgs,
+  nixpkgs-noto-variable,
+  nix-packages,
+  ...
 }:
 with pkgs; let
-  noto-variable = import nixpkgs-noto-variable { inherit system; };
-in
-{
+  noto-variable = import nixpkgs-noto-variable {inherit system;};
+in {
   nixpkgs.overlays = [
     (self: prev: {
       hydra-unstable = nix-packages.packages.${system}.hydra;
@@ -56,9 +55,30 @@ in
           sed 's/getBoolAttr."allowSubstitutes", true./true/' src/libstore/parsed-derivations.cc
         '';
       });
-      rnix-lsp = rnix-lsp.packages.${system}.rnix-lsp.override {
-        cargoBuildOptions = [ "--no-default-features" "--features" "alejandra" ];
-      };
+      rnix-lsp = with prev;
+        rustPlatform.buildRustPackage rec {
+          pname = "rnix-lsp";
+          version = "0.3.0-alejandra";
+
+          src = fetchFromGitHub {
+            owner = "nix-community";
+            repo = "rnix-lsp";
+            # https://github.com/nix-community/rnix-lsp/pull/89
+            rev = "9189b50b34285b2a9de36a439f6c990fd283c9c7";
+            sha256 = "sha256-ZnUtvwkcz7QlAiqQxhI4qVUhtVR+thLhG3wQlle7oZg=";
+          };
+
+          cargoSha256 = "sha256-VhE+DspQ0IZKf7rNkERA/gD7iMzjW4TnRSnYy1gdV0s=";
+          cargoBuildFlags = ["--no-default-features" "--features" "alejandra"];
+
+          checkInputs = lib.optional (!stdenv.isDarwin) nix;
+
+          meta = with lib; {
+            description = "A work-in-progress language server for Nix, with syntax checking and basic completion";
+            license = licenses.mit;
+            maintainers = with maintainers; [ma27];
+          };
+        };
     })
   ];
 }
