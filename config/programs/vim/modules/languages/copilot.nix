39fc667233ed22b9fa376706a7f0783ca8e23e80
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-08-09 20:37:47 +0100

    add copilot

---
+++ config/programs/vim/modules/languages/copilot.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  vim.g = {
+    copilot_filetypes = {
+      "*" = true;
+    };
+    copilot_node_command = "${pkgs.nodejs-16_x}/bin/node";
+  };
+  output.plugins = with pkgs.vimPlugins; [ copilot-vim ];
+}
