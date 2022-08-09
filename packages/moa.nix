2034b5667dd21b7d06042a6b706f273a0940b109
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-08-09 15:41:37 +0100

    remove moa.nix

--- packages/moa.nix
+++
@@ -1,7 +0,0 @@
-{fetchFromGitLab}:
-fetchFromGitLab {
-  owner = "fedstoa";
-  repo = "moa";
-  rev = "8c794b2cb854c1b25570fbb3082e83a9a088c773";
-  sha256 = "sha256-VhX0NsELBrG88qWT3XREQFQqZ5526mXiN5xIB8gRryM=";
-}
