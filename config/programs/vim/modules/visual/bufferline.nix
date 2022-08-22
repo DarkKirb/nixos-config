0d2a445582e732fd40b7d1f6bae09584296386cf
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-08-22 16:46:11 +0100

    fix diagnostics in bufferline

--- config/programs/vim/modules/visual/bufferline.nix
+++ config/programs/vim/modules/visual/bufferline.nix
@@ -55,16 +55,18 @@
     set tabline=
     lua << EOF
     require("bufferline").setup{
-      diagnostics = "nvim_lsp",
-      diagnostics_indicator = function(count, level, diagnostics_dict, context)
-        local s = " "
-        for e, n in pairs(diagnostics_dict) do
-          local sym = e == "error" and " "
-            or (e == "warning" and " " or "" )
-          s = s .. n .. sym
+      options = {
+        diagnostics = "nvim_lsp",
+        diagnostics_indicator = function(count, level, diagnostics_dict, context)
+          local s = " "
+          for e, n in pairs(diagnostics_dict) do
+            local sym = e == "error" and " "
+              or (e == "warning" and " " or "" )
+            s = s .. n .. sym
+          end
+          return s
         end
-        return s
-      end
+      }
     }
     EOF
     '';
