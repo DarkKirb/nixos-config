05cee346d82939caebaa85975901324e380366db
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-09-12 16:13:53 +0100

    switch away to fluffychat

--- config/programs/ims.nix
+++ config/programs/ims.nix
@@ -1,12 +1,10 @@
 {
   config,
   pkgs,
-  nixpkgs-fluffychat,
   ...
-}: let
-  pkgs-fluffychat = import nixpkgs-fluffychat {inherit (pkgs) system;};
-in {
+}: {
   home.packages = with pkgs; [
-    (pkgs-fluffychat.callPackage ./fluffychat.nix {})
+    element-desktop
   ];
 }
+
