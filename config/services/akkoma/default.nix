abcb00636546b62f680ec64204a0fcbd2d5e1b1e
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-09-15 11:36:55 +0100

    add blob raccoon emoji

--- config/services/akkoma/default.nix
+++ config/services/akkoma/default.nix
@@ -4,18 +4,25 @@
   config,
   ...
 }: let
+  raccoon-emoji = pkgs.fetchzip {
+    url = "https://volpeon.ink/art/emojis/raccoon/raccoon.zip";
+    sha256 = "b1986c87e44efcf08abaf2bb818f319e8d3dceb9f2f1a49d50e1c5bfc4434e64";
+  };
   static_dir = pkgs.stdenvNoCC.mkDerivation {
     name = "akkoma-static";
     src = pkgs.emptyDirectory;
     nativeBuildInputs = with pkgs; [xorg.lndir];
     akkoma_fe = nix-packages.packages.${pkgs.system}.akkoma-fe;
     akkoma_admin_fe = nix-packages.packages.${pkgs.system}.akkoma-admin-fe;
+    raccoon_emoji = raccoon-emoji;
     dontUnpack = false;
     installPhase = ''
       mkdir -p $out/frontends/pleroma-fe/stable
       lndir $akkoma_fe $out/frontends/pleroma-fe/stable
       mkdir -p $out/frontends/admin-fe/stable
       lndir $akkoma_admin_fe $out/frontends/admin-fe/stable
+      mkdir -p $out/emoji/raccoons
+      lndir $raccoon_emoji $out/emoji/raccoons
     '';
   };
   akkconfig = builtins.replaceStrings ["%AKKOMA_STATIC_DIR%"] ["${static_dir}"] (builtins.readFile ./akkoma.exs);
