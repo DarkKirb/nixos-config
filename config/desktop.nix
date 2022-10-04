fa856b01b2486be81be04f026d171983db4912a5
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-10-04 08:57:12 +0100

    dont use latest linux after all

--- config/desktop.nix
+++ config/desktop.nix
@@ -103,5 +103,5 @@ in {
     xkbVariant = "neo";
   };
   programs.sway.enable = true;
-  boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+  boot.kernelPackages = pkgs.linuxPackages_xanmod_tt;
 }
