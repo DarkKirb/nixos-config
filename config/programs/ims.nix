c9524af2cecfa748f68f09be486c5017891cc16b
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-10-18 12:25:05 +0100

    add ims back

--- config/programs/ims.nix
+++ config/programs/ims.nix
@@ -2,8 +2,15 @@
   config,
   pkgs,
   ...
-}: {
+}: 
+let 
+  firefox-wrapped = config.programs.firefox.package;
+  firefox = firefox-wrapped.unwrapped;
+  nss = pkgs.lib.lists.findFirst (x: x.pname == "nss") null firefox.build-inputs;
+in {
   home.packages = with pkgs; [
+    (discord.override { inherit nss; })
+    telegram-desktop
     element-desktop
   ];
 }
