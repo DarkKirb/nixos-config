4038c64a58c9d316d40693ae3f9138c896b197db
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-10-18 12:27:34 +0100

    make it work pls

--- config/programs/ims.nix
+++ config/programs/ims.nix
@@ -6,7 +6,7 @@
 let 
   firefox-wrapped = config.programs.firefox.package;
   firefox = firefox-wrapped.unwrapped;
-  nss = pkgs.lib.lists.findFirst (x: x.pname == "nss") null firefox.build-inputs;
+  nss = pkgs.lib.lists.findFirst (x: x.pname == "nss") null firefox.buildInputs;
 in {
   home.packages = with pkgs; [
     (discord.override { inherit nss; })
