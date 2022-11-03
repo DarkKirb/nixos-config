e7e9e7fadc82e4bad932335cd3c6be6a92956cd3
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-11-03 20:06:22 +0100

    use pipewire in mpd

--- config/programs/mpd.nix
+++ config/programs/mpd.nix
@@ -1,7 +1,13 @@
 _: {
   services.mpd = {
     enable = true;
-    musicDirectory = /home/darkkirb/Music;
+    musicDirectory = "/home/darkkirb/Music";
+    extraConfig = ''
+      audio_output {
+        type "pipewire"
+        name "Pipewire"
+      }
+    '';
   };
   programs.ncmpcpp.enable = true;
 }
