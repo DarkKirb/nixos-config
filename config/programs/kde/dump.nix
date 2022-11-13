bdff3ef554176f5c77522d7eeeff70bedf80bd61
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-11-13 14:11:07 +0100

    disable baloo file indexing

--- config/programs/kde/dump.nix
+++ config/programs/kde/dump.nix
@@ -234,16 +234,19 @@
       "systemsettings.desktop"."screenlocker" = [];
     };
     files = {
+      "baloofilerc"."Basic Settings"."Indexing-Enabled" = false;
       "baloofilerc"."General"."dbVersion" = 2;
       "baloofilerc"."General"."exclude filters" = "*~,*.part,*.o,*.la,*.lo,*.loT,*.moc,moc_*.cpp,qrc_*.cpp,ui_*.h,cmake_install.cmake,CMakeCache.txt,CTestTestfile.cmake,libtool,config.status,confdefs.h,autom4te,conftest,confstat,Makefile.am,*.gcode,.ninja_deps,.ninja_log,build.ninja,*.csproj,*.m4,*.rej,*.gmo,*.pc,*.omf,*.aux,*.tmp,*.po,*.vm*,*.nvram,*.rcore,*.swp,*.swap,lzo,litmain.sh,*.orig,.histfile.*,.xsession-errors*,*.map,*.so,*.a,*.db,*.qrc,*.ini,*.init,*.img,*.vdi,*.vbox*,vbox.log,*.qcow2,*.vmdk,*.vhd,*.vhdx,*.sql,*.sql.gz,*.ytdl,*.class,*.pyc,*.pyo,*.elc,*.qmlc,*.jsc,*.fastq,*.fq,*.gb,*.fasta,*.fna,*.gbff,*.faa,po,CVS,.svn,.git,_darcs,.bzr,.hg,CMakeFiles,CMakeTmp,CMakeTmpQmake,.moc,.obj,.pch,.uic,.npm,.yarn,.yarn-cache,__pycache__,node_modules,node_packages,nbproject,core-dumps,lost+found";
       "baloofilerc"."General"."exclude filters version" = 8;
-      "baloofilerc"."General"."exclude folders[$e]" = "$HOME/";
+      "baloofilerc"."General"."exclude folders[$e]" = "$HOME/,$HOME/";
       "baloofilerc"."General"."only basic indexing" = true;
       "dolphinrc"."KFileDialog Settings"."Places Icons Auto-resize" = false;
       "dolphinrc"."KFileDialog Settings"."Places Icons Static Size" = 22;
       "kactivitymanagerdrc"."activities"."b4547c33-271a-43a2-a926-5dbfc7a22859" = "Default";
       "kactivitymanagerdrc"."main"."currentActivity" = "b4547c33-271a-43a2-a926-5dbfc7a22859";
+      "kcminputrc"."Mouse"."X11LibInputXAccelProfileFlat" = false;
       "kcminputrc"."Tmp"."update_info" = "delete_cursor_old_default_size.upd:DeleteCursorOldDefaultSize";
+      "kded5rc"."Module-browserintegrationreminder"."autoload" = false;
       "kded5rc"."Module-device_automounter"."autoload" = false;
       "kded5rc"."PlasmaBrowserIntegration"."shownCount" = 4;
       "kdeglobals"."General"."AccentColor" = "233,58,154";
@@ -810,7 +813,7 @@
       "khotkeysrc"."WM"."inactiveBackground[$d]" = "";
       "khotkeysrc"."WM"."inactiveBlend[$d]" = "";
       "khotkeysrc"."WM"."inactiveForeground[$d]" = "";
-      "krunnerrc"."Plugins"."baloosearchEnabled" = true;
+      "krunnerrc"."Plugins"."baloosearchEnabled" = false;
       "kwalletrc"."Wallet"."First Use" = false;
       "kwinrc"."Compositing"."OpenGLIsUnsafe" = false;
       "kwinrc"."Desktops"."Id_1" = "449f29dc-7bbd-4c77-868a-a2fb194c9763";
