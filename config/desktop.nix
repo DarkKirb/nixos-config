0fe388055c2ec8edcb0c472419dd930099990cce
Charlotte 🦝 Delenk <lotte@chir.rs>
2022-12-31 10:17:51 +0100

    Revert "unblock kdeconnect"
    This reverts commit ac546e06448db04af3ac50a38f7f324262d9b02c.

--- config/desktop.nix
+++ config/desktop.nix
@@ -88,20 +88,6 @@ in {
   networking.firewall.allowedTCPPorts = [22000];
   networking.firewall.allowedUDPPorts = [22000];
 
-  # For KDEConnect
-  networking.firewall.allowedTCPPortRanges = [
-    {
-      from = 1714;
-      to = 1764;
-    }
-  ];
-  networking.firewall.allowedUDPPortRanges = [
-    {
-      from = 1714;
-      to = 1764;
-    }
-  ];
-
   environment.systemPackages = with pkgs; [
     pinentry-gtk2
   ];
