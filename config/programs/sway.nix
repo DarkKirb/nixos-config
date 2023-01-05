050fabaa698252ecdf602a9f982320ea22e6c880
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-01-05 17:50:35 +0100

    disable wayland in sdl (doesn’t work)

--- config/programs/sway.nix
+++ config/programs/sway.nix
@@ -147,7 +147,6 @@ in {
     };
     wrapperFeatures.gtk = true;
     extraSessionCommands = ''
-      export SDL_VIDEODRIVER=wayland
       export QT_QPA_PLATFORM=wayland
       export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
       export _JAVA_AWT_WM_NONREPARENTING=1
