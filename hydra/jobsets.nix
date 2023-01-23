4a9a81c102480d6a2ddab10aeb6e596b289a875e
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-01-23 14:51:09 +0100

    h

--- hydra/jobsets.nix
+++ hydra/jobsets.nix
@@ -6,26 +6,27 @@
   prs = builtins.fromJSON (builtins.readFile prsJSON);
 
   jobsets =
-    pkgs.lib.mapAttrs (num: info:
-      {
-        enabled = 1;
-        hidden = false;
-        description = "PR ${num}: ${info.title}";
-        nixexprinput = "nixos-config";
-        nixexprpath = "hydra/default.nix";
-        checkinterval = 300;
-        schedulingshares = 100;
-        enableemail = false;
-        emailoverride = "";
-        keepnr = 1;
-        inputs = {
-          nixos-config = {
-            type = "git";
-            value = "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}";
-            emailresponsible = false;
+    (pkgs.lib.mapAttrs (
+        num: info: {
+          enabled = 1;
+          hidden = false;
+          description = "PR ${num}: ${info.title}";
+          nixexprinput = "nixos-config";
+          nixexprpath = "hydra/default.nix";
+          checkinterval = 300;
+          schedulingshares = 100;
+          enableemail = false;
+          emailoverride = "";
+          keepnr = 1;
+          inputs = {
+            nixos-config = {
+              type = "git";
+              value = "https://github.com/${info.head.repo.owner.login}/${info.head.repo.name}.git ${info.head.ref}";
+              emailresponsible = false;
+            };
           };
-        };
-      }
+        }
+      )
       prs)
     // {
       nixos-config = {
