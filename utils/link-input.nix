928752bc5d4449bd0d6626af59becd6138dd3ad9
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-09 16:45:30 +0100

    patch default nixpkgs

--- utils/link-input.nix
+++ utils/link-input.nix
@@ -1,6 +1,19 @@
-inputs: {lib, ...}: let
+inputs': {
+  system,
+  lib,
+  ...
+}: let
   # Taken from https://github.com/gytis-ivaskevicius/flake-utils-plus/blob/master/lib/options.nix
   inherit (lib) filterAttrs mapAttrs';
+  inputs =
+    inputs'
+    // {
+      nixpkgs = import ./patched-nixpkgs.nix {
+        inherit (inputs') nixpkgs;
+        inherit system;
+        patches = "${inputs'.self}/extra/nixpkgs.patch";
+      };
+    };
   flakes = filterAttrs (name: value: value ? outputs) inputs;
   nixRegistry =
     builtins.mapAttrs
