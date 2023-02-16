88f87e59e7b4faac24fed3c008734e8c109d5aa9
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-16 16:24:19 +0100

    fix typo

--- packages/minecraft/dynmap.nix
+++ packages/minecraft/dynmap.nix
@@ -2,6 +2,6 @@
   core = fetchurl {
     url = "https://dev.bukkit.org/projects/dynmap/files/4371728/download";
     sha256 = "12n0s4qhc5gbsw649jpjyn7x4g4ilkbqq188l2r4wpwp38l134a0";
-    name = "Dynmap.jar"
+    name = "Dynmap.jar";
   };
 }
