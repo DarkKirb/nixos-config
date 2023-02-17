715647978ce4369e1582b3e2a7b6753f57e6ca97
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-17 15:07:36 +0100

    add bcachefs support to the installer

--- config/installer.nix
+++ config/installer.nix
@@ -10,14 +10,15 @@
   networking.wireguard.interfaces."wg0".ips = [
     "fd0d:a262:1fa6:e621:6ec2:1e4e:ce7f:d2af/64"
   ];
-  boot.supportedFilesystems = ["zfs"];
+  boot.supportedFilesystems = ["zfs" "bcachefs"];
+  system.packages = with pkgs; ["bcachefs-tools"];
   networking.hostId = "8425e349";
   # Oldest system I have is skylake-based
   /*
-  nixpkgs.localSystem = {
-  gcc.arch = "skylake";
-  gcc.tune = "skylake";
-  system = "x86_64-linux";
-  };
-  */
+   nixpkgs.localSystem = {
+   gcc.arch = "skylake";
+   gcc.tune = "skylake";
+   system = "x86_64-linux";
+   };
+   */
 }
