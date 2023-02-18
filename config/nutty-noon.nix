f43d518313acea8c1da18d168799171e3fb81900
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-02-18 10:24:30 +0100

    fix typos

--- config/nutty-noon.nix
+++ config/nutty-noon.nix
@@ -34,7 +34,7 @@
   boot.supportedFilesystems = lib.mkForce ["bcachefs" "vfat"];
   
   fileSystems."/" = {
-    device = "/dev/disk/by-partuuid/53773b73-fb8a-4de8-ac58-d9d8ff1be430:/dev/disk/by-paruuid/d4c6a94f-2ae9-e446-9613-2596c564078c";
+    device = "/dev/disk/by-partuuid/d4c6a94f-2ae9-e446-9613-2596c564078c:/dev/disk/by-partuuid/53773b73-fb8a-4de8-ac58-d9d8ff1be430";
     fsType = "bcachefs";
   };
 
