4e94a6871216f7762bd94a0a9fac4b860d61ccff
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-03-11 13:49:43 +0100

    fix eval

--- config/services/chir.rs/auth.nix
+++ config/services/chir.rs/auth.nix
@@ -22,7 +22,7 @@
         , database_url = "postgres://auth_chir_rs:${d}{password}@nixos-8gb-fsn1-1.int.chir.rs"
         , listen_addr = "[::1]:7954"
         , redis_url = "redis://${d}{password}@nixos-8gb-fsn1-1.int.chir.rs:53538/0"
-        , asset_path = "${chir-rs.packages.${system}.chir-rs-web}"
+        , asset_path = "${chir-rs.packages.${system}.chir-rs-auth-web}"
         }
   '';
 in {
