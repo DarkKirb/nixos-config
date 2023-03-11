012bf85461db4a958b75ae3abbe3c4b4074d614d
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-03-11 17:52:07 +0100

    fix listen address

--- config/services/chir.rs/auth.nix
+++ config/services/chir.rs/auth.nix
@@ -20,7 +20,7 @@
 
     in  BaseConfig::{
         , database_url = "postgres://auth_chir_rs:${d}{password}@nixos-8gb-fsn1-1.int.chir.rs"
-        , listen_addr = "[::1]:7954"
+        , listen_addr = "127.0.0.1:7954"
         , redis_url = "redis://${d}{password}@nixos-8gb-fsn1-1.int.chir.rs:53538/0"
         , asset_path = "${chir-rs.packages.${system}.chir-rs-auth-web}"
         }
@@ -75,7 +75,7 @@ in {
     extraConfig = ''
       import baseConfig
 
-      reverse_proxy http://127.0.0.1:5621 {
+      reverse_proxy http://127.0.0.1:7954 {
         trusted_proxies private_ranges
       }
     '';
