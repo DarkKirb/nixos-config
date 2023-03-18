a94b94f929c434fe9b7e4c78e969efa267ca1bd1
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-03-18 18:45:07 +0100

    use netboot-base

--- config/netboot.nix
+++ config/netboot.nix
@@ -5,7 +5,7 @@
   ...
 }: {
   imports = [
-    "${nixpkgs}/nixos/modules/installer/netboot/netboot-minimal.nix"
+    "${nixpkgs}/nixos/modules/installer/netboot/netboot-base.nix"
   ];
   networking.wireguard.interfaces."wg0".ips = [
     "fd0d:a262:1fa6:e621:6ec2:1e4e:ce7f:d2af/64"
