c07cc43df8aacabc5af563a444e8160063953f61
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-03-28 05:42:14 +0100

    idk why this is broken

--- config/devterm.nix
+++ config/devterm.nix
@@ -16,14 +16,14 @@
   ];
 
   hardware.raspberry-pi."4" = {
-    audio.enable = true;
+    #audio.enable = true;
     dwc2.enable = true;
-    i2c0.enable = true;
-    i2c1.enable = true;
-    fkms-3d.enable = true;
-    #apply-overlays-dtmerge.enable = true;
+    #i2c0.enable = true;
+    #i2c1.enable = true;
+    #fkms-3d.enable = true;
+    apply-overlays-dtmerge.enable = true;
     pwm0.enable = true;
-    tc358743.enable = true;
+    #tc358743.enable = true;
   };
 
   fileSystems = {
