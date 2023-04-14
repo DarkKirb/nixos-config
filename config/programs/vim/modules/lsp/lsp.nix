b1be5a910c7868c8a38e4ce9ffa3d3b0324b6fca
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-04-14 07:44:43 +0100

    add lsp keybindings

--- config/programs/vim/modules/lsp/lsp.nix
+++ config/programs/vim/modules/lsp/lsp.nix
@@ -28,6 +28,85 @@ in {
       if config.isDesktop
       then [pkgs.vimPlugins.nvim-lspconfig]
       else [];
+    vim.keybindings.keybindings =
+      if config.isDesktop
+      then {
+        "<leader>".s = {
+          e = {
+            command = "<cmd>lua vim.diagnostic.open_float()<cr>";
+            label = "Open diagnostic float";
+          };
+          "[" = {
+            command = "<cmd>lua vim.diagnostic.goto_prev()<cr>";
+            label = "Prev diagnostic";
+          };
+          "]" = {
+            command = "<cmd>lua vim.diagnostic.goto_next()<cr>";
+            label = "Next diagnostic";
+          };
+          q = {
+            command = "<cmd>lua vim.diagnostic.setloclist<cr>";
+            label = "Add buffer diagnostics to loc list";
+          };
+          K = {
+            command = "<cmd>lua vim.lsp.buf.hover()<cr>";
+            label = "Hover";
+          };
+          "<C-k>" = {
+            command = "<cmd>lua vim.lsp.buf.signature_help()<cr>";
+            label = "Signature help";
+          };
+          w = {
+            a = {
+              command = "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>";
+              label = "Add workspace folder";
+            };
+            r = {
+              command = "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>";
+              label = "Remove workspace folder";
+            };
+            l = {
+              command = "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>";
+              label = "List workspace folders";
+            };
+          };
+          D = {
+            command = "<cmd>lua vim.lsp.buf.type_definition()<cr>";
+            label = "Show type definition";
+          };
+          r.n = {
+            command = "<cmd>lua vim.lsp.buf.rename()<cr>";
+            label = "Rename buffer";
+          };
+          c.a = {
+            command = "<cmd>lua vim.lsp.buf.code_action()<cr>";
+            label = "Code action";
+          };
+          f = {
+            command = "<cmd>lua vim.lsp.buf.format({asyync = true})<cr>";
+            label = "Format buffer";
+          };
+        };
+        g = {
+          D = {
+            command = "<cmd>lua vim.lsp.buf.declaration()<cr>";
+            label = "Declaration";
+          };
+          d = {
+            command = "<cmd>lua vim.lsp.buf.definition()<cr>";
+            label = "Definition";
+          };
+          i = {
+            command = "<cmd>lua vim.lsp.buf.implementation()<cr>";
+            label = "Implementation";
+          };
+          r = {
+            command = "<cmd>lua vim.lsp.buf.references()<cr>";
+            label = "References";
+          };
+        };
+      }
+      else {};
   };
 
   options.lspconfig = mkOption {
