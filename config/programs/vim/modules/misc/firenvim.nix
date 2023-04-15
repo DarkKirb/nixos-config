3cf7e73e46cd9673e76a6952371ef17b5897c564
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-04-15 10:46:24 +0100

    hotfix firenvim

--- config/programs/vim/modules/misc/firenvim.nix
+++ config/programs/vim/modules/misc/firenvim.nix
@@ -11,10 +11,12 @@
     if config.isDesktop
     then ''
       silent call firenvim#install(0)
+      lua << EOF
       if vim.g.started_by_firenvim then
         vim.o.showtabline = 0
         require("lualine").hide()
       end
+      EOF
     ''
     else "";
   vim.opt.guifont = "Fira_Code_Mono_Nerd_Font_Mono:h9";
