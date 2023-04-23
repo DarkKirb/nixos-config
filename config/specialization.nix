c788c898c8566b0fe9348caf43ebd9e45af2486a
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-04-23 06:11:06 +0100

    use userHook

--- config/specialization.nix
+++ config/specialization.nix
@@ -1,15 +1,12 @@
 # Configuration file configuring specialization
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}: {
-  nixpkgs.overlays = [
-    (self: prev: {
-    })
-  ];
+{config, ...}: {
   environment.etc."nix/local-system.json".text = builtins.toJSON {
     inherit (config.nixpkgs.localSystem) system gcc rustc linux-kernel;
   };
+
+  # Instead of overriding stdenv, we provide a user hook that appends the correct configure flags
+  nixpkgs.config.stdenv.userHook = ''
+    NIX_CFLAGS_COMPILE+="-O3 -pipe -flto"
+    unset doCheck doInstallCheck
+  '';
 }
