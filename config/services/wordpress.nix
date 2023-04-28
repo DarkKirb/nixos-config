e9890b682374449dff95dbac5e207f6ee7a0b022
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-04-28 08:34:50 +0100

    update wordpress config

--- config/services/wordpress.nix
+++ config/services/wordpress.nix
@@ -19,7 +19,55 @@
     extraConfig = ''
       $_SERVER['HTTPS']='on';
     '';
+
+    plugins = {
+      inherit
+        (pkgs.wordpress-plugins)
+        google-sitemap-generator
+        indieweb
+        pubsubhubbub
+        indieweb-post-kinds
+        indeiauth
+        syndication-links
+        micropub
+        webmention
+        activitypub
+        friends
+        hum
+        webfinger
+        nodeinfo
+        classic-editor
+        wordpress-seo
+        ilab-media-tools
+        translatepress-multilingual
+        webp-express
+        jetpack
+        ;
+    };
+    themes = {
+      inherit
+        (pkgs.wordpress-themes)
+        sempress
+        twentytwentythree
+        ;
+    };
   };
+
+  nixpkgs.overlays = [
+    (self: super: {
+      wordpress = super.wordpress.overrideAttrs (oldAttrs: {
+        installPhase =
+          oldAttrs.installPhase
+          + ''
+            ln -s /var/lib/wordpress/lotte.chir.rs/webp-express $out/share/wordpress/wp-content/webp-express
+          '';
+      });
+    })
+  ];
+
+  systemd.tmpfiles.rules = [
+    "d '/var/lib/wordpress/lotte.chir.rs/webp-express' 0750 wordpress acme - -"
+  ];
 
   services.caddy.virtualHosts."lotte.chir.rs" = {
     useACMEHost = "chir.rs";
