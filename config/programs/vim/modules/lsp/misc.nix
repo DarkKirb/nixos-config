da55fd1b7b6d9ccb963eb10a60cd295e985533b4
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-06-12 16:18:31 +0100

    disable ltex on riscv

--- config/programs/vim/modules/lsp/misc.nix
+++ config/programs/vim/modules/lsp/misc.nix
@@ -1,44 +1,56 @@
 {pkgs, ...}: {
-  lspconfigPath = with pkgs; [
-    nodePackages.bash-language-server
-    clang
-    clang-tools
-    cmake-language-server
-    dhall-lsp-server
-    nodePackages.dockerfile-language-server-nodejs
-    elixir-ls
-    gopls
-    haskell-language-server
-    nodePackages.vscode-css-languageserver-bin
-    nodePackages.vscode-html-languageserver-bin
-    nodePackages.vscode-json-languageserver
-    ltex-ls
-    lua-language-server
-    marksman
-    nil
-    pyright
-    nodePackages.typescript
-    nodePackages.typescript-language-server
-    nodePackages.vim-language-server
-  ];
-  lspconfig = {
-    bashls = {};
-    clangd = {};
-    cmake = {};
-    cssls = {};
-    dhall_lsp_server = {};
-    dockerls = {};
-    elixirls.cmd = ["elixir-ls"];
-    gopls = {};
-    hls.filetypes = ["haskell" "lhaskell" "cabal"];
-    html = {};
-    jsonls = {};
-    ltex = {};
-    lua_ls = {};
-    marksman = {};
-    nil_ls = {};
-    pyright = {};
-    tsserver = {};
-    vimls = {};
-  };
+  lspconfigPath = with pkgs;
+    [
+      nodePackages.bash-language-server
+      clang
+      clang-tools
+      cmake-language-server
+      dhall-lsp-server
+      nodePackages.dockerfile-language-server-nodejs
+      elixir-ls
+      gopls
+      haskell-language-server
+      nodePackages.vscode-css-languageserver-bin
+      nodePackages.vscode-html-languageserver-bin
+      nodePackages.vscode-json-languageserver
+      lua-language-server
+      marksman
+      nil
+      pyright
+      nodePackages.typescript
+      nodePackages.typescript-language-server
+      nodePackages.vim-language-server
+    ]
+    ++ (
+      if pkgs.system != "riscv64-linux"
+      then with pkgs; [ltex-ls]
+      else []
+    );
+  lspconfig =
+    {
+      bashls = {};
+      clangd = {};
+      cmake = {};
+      cssls = {};
+      dhall_lsp_server = {};
+      dockerls = {};
+      elixirls.cmd = ["elixir-ls"];
+      gopls = {};
+      hls.filetypes = ["haskell" "lhaskell" "cabal"];
+      html = {};
+      jsonls = {};
+      lua_ls = {};
+      marksman = {};
+      nil_ls = {};
+      pyright = {};
+      tsserver = {};
+      vimls = {};
+    }
+    // (
+      if pkgs.system != "riscv64-linux"
+      then {
+        ltex = {};
+      }
+      else {}
+    );
 }
