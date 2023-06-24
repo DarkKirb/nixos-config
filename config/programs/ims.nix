643d4bcf65e456bf790742d9cf79235e9239b3e8
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-06-24 12:45:07 +0100

    use nheko on non-x86

--- config/programs/ims.nix
+++ config/programs/ims.nix
@@ -1,6 +1,9 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [
-    cinny-desktop
-    element-desktop
-  ];
+  home.packages = with pkgs;
+    if system == "x86_64-linux"
+    then [
+      cinny-desktop
+      element-desktop
+    ]
+    else [nheko];
 }
