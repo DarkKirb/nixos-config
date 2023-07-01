fbdeb04ffc5e031dfe5a8b58148872ce0bf40804
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-07-01 12:21:45 +0100

    reinstall nutty-noon

--- config/netboot.nix
+++ config/netboot.nix
@@ -6,12 +6,13 @@
 }: {
   imports = [
     "${nixpkgs}/nixos/modules/installer/netboot/netboot-base.nix"
+    ../modules/bcachefs.nix
   ];
   networking.wireguard.interfaces."wg0".ips = [
     "fd0d:a262:1fa6:e621:6ec2:1e4e:ce7f:d2af/64"
   ];
   boot.supportedFilesystems = lib.mkForce ["bcachefs" "vfat"];
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_testing_bcachefs;
+  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.linux-bcachefs);
   networking.hostId = "8425e349";
   nix.settings.post-build-hook = lib.mkForce "true";
 }
