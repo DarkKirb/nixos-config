3ca2eda4b84f433dc85a30c568ab7a0e6583c97b
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-07-16 06:46:35 +0100

    hotfix

--- config/programs/mpd.nix
+++ config/programs/mpd.nix
@@ -9,10 +9,10 @@ _: {
       }
       replaygain          "track"
       volume_normalization "yes"
-      max_playlist_length 1048576
-      max_command_list_size 1048576
-      max_output_buffer_size 1048576
-      auto_update yes
+      max_playlist_length "1048576"
+      max_command_list_size "1048576"
+      max_output_buffer_size "1048576"
+      auto_update "yes"
     '';
   };
   programs.ncmpcpp.enable = true;
