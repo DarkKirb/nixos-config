8eeb7aeef34b72f13c329ebacdca180c6696025c
Charlotte 🦝 Delenk <lotte@chir.rs>
2023-10-30 10:36:45 +0100

    add UAC support to wine

--- config/games/default.nix
+++ config/games/default.nix
@@ -5,7 +5,17 @@ args: {
   ...
 }: {
   home.packages = [
-    nix-gaming.packages.x86_64-linux.wine-ge
+    (nix-gaming.packages.x86_64-linux.wine-ge.overrideAttrs (super: {
+      patches = super.patches or [] ++ [
+        ./wine/server-default_integrity/0001-server-Create-processes-using-a-limited-administrato.patch
+        ./wine/server-default_integrity/0002-shell32-Implement-the-runas-verb.patch
+        ./wine/server-default_integrity/0003-wine.inf-Set-the-EnableLUA-value-to-1.patch
+        ./wine/server-default_integrity/0004-msi-Create-the-custom-action-server-as-an-elevated-p.patch
+        ./wine/server-default_integrity/0005-ntdll-Always-start-the-initial-process-through-start.patch
+        ./wine/server-default_integrity/0006-kernelbase-Elevate-processes-if-requested-in-CreateP.patch
+        ./wine/server-default_integrity/0007-ntdll-Elevate-processes-if-requested-in-RtlCreateUse.patch
+      ];
+    }))
     pkgs.xivlauncher
     pkgs.prismlauncher
     pkgs.mgba
