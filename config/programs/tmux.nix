30de3769a33011f707b04aeb48de2bd3fd1996d1
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-04-01 15:30:26 +0100

    add tmux

--- config/programs/tmux.nix
+++ config/programs/tmux.nix
@@ -5,5 +5,19 @@
     clock24 = true;
     prefix = "C-a";
     sensibleOnTop = true;
+    plugins = with pkgs.tmuxPlugins; [
+        power-theme
+        cpu
+        {
+            plugin = resurrect;
+            extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        }
+        {
+            plugin = continuum;
+            extraConfig = ''
+                set -g @continuum-restore 'on'
+            '';
+        }
+    ];
   };
 }
