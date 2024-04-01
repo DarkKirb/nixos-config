7f29e7683d2d5cd15d43a9be33d8e3d7f6eabbb3
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-04-01 15:32:02 +0100

    format

--- config/programs/tmux.nix
+++ config/programs/tmux.nix
@@ -6,18 +6,18 @@
     prefix = "C-a";
     sensibleOnTop = true;
     plugins = with pkgs.tmuxPlugins; [
-        power-theme
-        cpu
-        {
-            plugin = resurrect;
-            extraConfig = "set -g @resurrect-strategy-nvim 'session'";
-        }
-        {
-            plugin = continuum;
-            extraConfig = ''
-                set -g @continuum-restore 'on'
-            '';
-        }
+      power-theme
+      cpu
+      {
+        plugin = resurrect;
+        extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+        '';
+      }
     ];
   };
 }
