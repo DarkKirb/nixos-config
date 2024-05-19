ffd0d21c7c5e0e2fa1195212dc50e760817cbde3
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-05-19 06:39:56 +0100

    remove broken options in loki config

--- config/services/loki.nix
+++ config/services/loki.nix
@@ -20,7 +20,6 @@ _: {
         max_chunk_age = "1h";
         chunk_target_size = 999999;
         chunk_retain_period = "30s";
-        max_transfer_retries = 0;
       };
 
       schema_config = {
@@ -43,7 +42,6 @@ _: {
           active_index_directory = "/var/lib/loki/boltdb-shipper-active";
           cache_location = "/var/lib/loki/boltdb-shipper-cache";
           cache_ttl = "24h";
-          shared_store = "filesystem";
         };
 
         filesystem = {
@@ -56,10 +54,6 @@ _: {
         reject_old_samples_max_age = "168h";
       };
 
-      chunk_store_config = {
-        max_look_back_period = "0s";
-      };
-
       table_manager = {
         retention_deletes_enabled = false;
         retention_period = "0s";
@@ -67,7 +61,6 @@ _: {
 
       compactor = {
         working_directory = "/var/lib/loki";
-        shared_store = "filesystem";
         compactor_ring = {
           kvstore = {
             store = "inmemory";
