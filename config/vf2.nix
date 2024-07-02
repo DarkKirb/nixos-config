61994a499bd59c7ee78c4400cb5e558070bac414
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-07-02 19:30:05 +0200

    add extra installation commands for systemd-boot

--- config/vf2.nix
+++ config/vf2.nix
@@ -88,4 +88,14 @@
   };
 
   system.stateVersion = "24.05";
+
+  boot.loader.systemd-boot.extraInstallCommands = ''
+    set -euo pipefail
+    ${pkgs.coreutils}/bin/cp --no-preserve=mode -r ${config.hardware.deviceTree.package} ${config.boot.loader.efi.efiSysMountPoint}/
+    for filename in ${config.boot.loader.efi.efiSysMountPoint}/loader/entries/nixos*-generation-[1-9]*.conf; do
+      if ! ${pkgs.gnugrep}/bin/grep -q 'devicetree' $filename; then
+        ${pkgs.coreutils}/bin/echo "devicetree /dtbs/${config.hardware.deviceTree.name}" >> $filename
+      fi
+    done
+  '';
 }
