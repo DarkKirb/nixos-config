8a30ce0344818d3c48938a0dc85597b3d0a992a0
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-07-04 11:44:59 +0200

    add raspberry pi 2

--- config/rpi2.nix
+++ config/rpi2.nix
@@ -1,15 +1,42 @@
-_: {
+{
+  pkgs,
+  lib,
+  ...
+} @ args: {
   networking.hostName = "rpi2";
   networking.hostId = "29d7b964";
-  # NixOS wants to enable GRUB by default
-  boot.loader.grub.enable = false;
-  # Enables the generation of /boot/extlinux/extlinux.conf
-  boot.loader.generic-extlinux-compatible.enable = true;
   fileSystems."/" = {
     device = "/dev/disk/by-label/NIXOS_SD";
     fsType = "ext4";
   };
-  system.stateVersion = "21.11";
-  home-manager.users.darkkirb = import ./home-manager/darkkirb.nix false;
+  system.stateVersion = "24.05";
+  home-manager.users.darkkirb = import ./home-manager/darkkirb.nix {
+    desktop = false;
+    inherit args;
+  };
   nix.settings.cores = 4;
+  nixpkgs.crossSystem = {
+    config = "armv7l-unknown-linux-gnueabihf";
+    system = "armv7l-linux";
+  };
+  boot = {
+    consoleLogLevel = lib.mkDefault 7;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_rpi2;
+    kernelParams = [
+      "dwc_otg.lpm_enable=0"
+      "console=ttyAMA0,115200"
+      "rootwait"
+      "elevator=deadline"
+    ];
+    loader = {
+      grub.enable = lib.mkDefault false;
+      generationsDir.enable = lib.mkDefault false;
+      raspberryPi = {
+        enable = lib.mkDefault true;
+        version = lib.mkDefault 2;
+      };
+    };
+  };
+  nixpkgs.config.platform = lib.systems.platforms.raspberrypi2;
+  powerManagement.enable = lib.mkDefault false;
 }
--- flake.nix
+++ flake.nix
@@ -162,6 +162,10 @@ rec {
         name = "devterm";
         system = "aarch64-linux";
       }
+      {
+        name = "rpi2";
+        system = "x86_64-linux";
+      }
     ];
     mkPackages = system: let
       pkgs = import nixpkgs {
