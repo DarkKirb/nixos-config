e5c22907eecb7b602ea2dab8f5b140b9ff4ce63c
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-07-17 07:53:51 +0200

    readd kitty

--- config/home-manager/base.nix
+++ config/home-manager/base.nix
@@ -20,17 +20,17 @@ desktop: {pkgs, ...}: {
         enable = true;
       };
       initExtraBeforeCompInit = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      /*
-        initExtra = ''
-        [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+      
+      initExtra = ''
+      [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-          test -n "$KITTY_INSTALLATION_DIR" || export KITTY_INSTALLATION_DIR=${pkgs.kitty}/lib/kitty
-          export KITTY_SHELL_INTEGRATION=enabled
-          autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
-          kitty-integration
-          unfunction kitty-integration
-      '';
-      */
+        test -n "$KITTY_INSTALLATION_DIR" || export KITTY_INSTALLATION_DIR=${pkgs.kitty}/lib/kitty
+        export KITTY_SHELL_INTEGRATION=enabled
+        autoload -Uz -- "$KITTY_INSTALLATION_DIR"/shell-integration/zsh/kitty-integration
+        kitty-integration
+        unfunction kitty-integration
+    '';
+      
       plugins = [
       ];
     };
@@ -49,11 +49,9 @@ desktop: {pkgs, ...}: {
       // (
         if desktop
         then {
-          /*
-            icat = "${pkgs.kitty}/bin/kitty +kitten icat";
+          icat = "${pkgs.kitty}/bin/kitty +kitten icat";
           d = "${pkgs.kitty}/bin/kitty +kitten diff";
           hg = "${pkgs.kitty}/bin/kitty +kitten hyperlinked_grep";
-          */
           cargo = "${pkgs.cargo-mommy}/bin/cargo-mommy";
         }
         else {}
