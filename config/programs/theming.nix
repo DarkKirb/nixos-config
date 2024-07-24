1f197b2526e8fe05c1050fafe41bc5c8d3d4b725
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-07-24 12:07:33 +0200

    fix stuff

--- config/programs/theming.nix
+++ config/programs/theming.nix
@@ -117,12 +117,12 @@ in {
     params = [];
   };
   wayland.windowManager.sway.config.output."*".bg = "${bgPng} fill";
-  dconf.settings."org/gnome/desktop/interface" = {
+  /*dconf.settings."org/gnome/desktop/interface" = {
     icon-theme = "breeze-dark";
     cursor-theme = "Vanilla-DMZ";
-  };
+  };*/
   gtk = {
-    enable = true;
+    #enable = true;
     gtk2.extraConfig = ''
       gtk-cursor-theme-name = "Vanilla-DMZ"
       gtk-cursor-theme-size = 0
@@ -151,7 +151,7 @@ in {
     };
   };
   qt = {
-    enable = true;
+    #enable = true;
     style = {
       name = "lightly";
       package = pkgs.plasma5Packages.lightly;
@@ -258,14 +258,14 @@ in {
     })
   ];
 
-  home.file = {
+  /*home.file = {
     ".icons/default/index.theme".text = ''
       [Icon Theme]
       Name=Default
       Comment=Default Cursor Theme
       Inherits=Vanilla-DMZ
     '';
-  };
+  };*/
   programs.kitty.settings = with theme; {
     background = color 0;
     foreground = color 15;
