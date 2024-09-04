3baceee3181bb3498cda34ee3b578ce1b65493d7
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-09-04 21:00:38 +0200

    add renovate

---
+++ config/services/renovate.nix
@@ -0,0 +1,31 @@
+{
+  pkgs,
+  config,
+  ...
+}: {
+  services.renovate = {
+    enable = true;
+    schedule = "hourly";
+    settings = {
+      platform = "gitea";
+      endpoint = "https://git.chir.rs";
+      gitAuthor = "Renovate <gitea-bot@chir.rs>";
+      autodiscover = true;
+      autodiscoverTopics = ["managed-by-renovate"];
+      nix.enabled = true;
+      lockFileMaintenance.enabled = true;
+      ossVulnerabilityAlerts = true;
+      credentials = {
+        RENOVATE_TOKEN = config.sops.secrets."services/renovate-token".path;
+      };
+    };
+    runtimePackages = with pkgs; [
+      config.nix.package
+      nodejs
+      corepack
+      cargo
+    ];
+  };
+
+  sops.secrets."services/renovate" = {};
+}
