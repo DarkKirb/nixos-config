2f383a331362a8b2ce450e80f16a69e5de386d0f
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-09-23 20:26:18 +0200

    add mautrix-slack

---
+++ config/services/mautrix-slack.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../../modules/matrix/mautrix-slack.nix
+  ];
+
+  services.mautrix-slack = {
+    enable = true;
+    environmentFile = config.sops.secrets."services/mautrix/shared_secret".path;
+    settings = {
+      bridge = {
+        permissions = {
+          "*" = "relay";
+          "@miifox:chir.rs" = "user";
+          "@lotte:chir.rs" = "admin";
+        };
+      };
+      database = {
+        type = "postgres";
+        uri = "postgres:///mautrix_slack?sslmode=disable&host=/run/postgresql";
+      };
+      homeserver = {
+        address = "https://matrix.chir.rs";
+        domain = "chir.rs";
+        async_media = true;
+      };
+      appservice = {
+        ephemeral_events = true;
+        async_transactions = true;
+      };
+      backfill = {
+        enabled = true;
+        queue.enabled = true;
+      };
+      encryption = {
+        allow = true;
+        default = true;
+        appservice = true;
+      };
+    };
+  };
+  sops.secrets."services/mautrix/shared_secret" = {};
+  services.postgresql.ensureDatabases = [
+    "mautrix_slack"
+  ];
+}
