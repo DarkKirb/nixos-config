afa31fe05bc3a031bd644451c47998de3624735b
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-10-29 08:09:53 +0100

    disable nix inside of containers

--- modules/containers/minimize.nix
+++ modules/containers/minimize.nix
@@ -3,4 +3,5 @@
     (nixpkgs.outPath + "/nixos/modules/profiles/minimal.nix")
     (nixpkgs.outPath + "/nixos/modules/profiles/headless.nix")
   ];
+  nix.enable = false; # We don’t need the nix package manager inside of the container.
 }
