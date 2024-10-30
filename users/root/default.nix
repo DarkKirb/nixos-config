4d33b5dfdeb9a5cee5276ccff0127942b986381d
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-10-30 07:45:16 +0100

    create home directory for root

--- users/root/default.nix
+++ users/root/default.nix
@@ -1,5 +1,6 @@
 {config, ...}: {
   users.users.root = {
+    createHome = true;
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDXQlfvRUm/z6eP1EjsajIbMibkq9n+ymlbBi7NFiOuaAAAABHNzaDo= ssh:"
     ];
