cf3548a8a2d95c7311f4d1b8a3dd2cfdcae5050e
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-10-31 10:10:04 +0100

    add gitMinimal if nix is enabled

--- modules/nix/lix.nix
+++ modules/nix/lix.nix
@@ -1,10 +1,16 @@
 {
   lix-module,
   inTester,
+  config,
+  pkgs,
   ...
 }: {
   imports =
     if inTester
     then []
     else [lix-module.nixosModules.default];
+
+  environment.systemPackages = mkIf config.nix.enable [
+    pkgs.gitMinimal
+  ];
 }
