c778f7ad7331b434781962a1f47b3aca5b623a7d
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-03 14:23:06 +0100

    add tide plugin to fish

--- programs/shell/fish/home-manager.nix
+++ programs/shell/fish/home-manager.nix
@@ -1,5 +1,8 @@
-{...}: {
+{pkgs, ...}: {
   programs.fish.enable = true;
+  programs.fish.plugins = with pkgs.fishPlugins; [
+    tide
+  ];
   home.persistence.default.files = [
     ".local/share/fish/fish_history"
   ];
