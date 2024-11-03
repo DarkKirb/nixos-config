756c156b26b941ac97215b09ff7968c78a103dc7
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-03 11:31:10 +0100

    fix eval

--- programs/shell/fish/default.nix
+++ programs/shell/fish/default.nix
@@ -1,6 +1,17 @@
-{config, ...}: {
+{pkgs, ...}: {
   programs.fish.enable = true;
-  home-manager.users.root = [
+  home-manager.users.root.imports = [
     ./home-manager.nix
   ];
+  programs.bash.interactiveShellInit = ''
+    for user in root; do
+      if [[ $USER == $user ]]; then
+        if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+        then
+          shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+          exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+        fi
+      fi
+    done
+  '';
 }
--- programs/shell/fish/home-manager.nix
+++ programs/shell/fish/home-manager.nix
@@ -1,10 +1,3 @@
-{pkgs, ...}: {
+{...}: {
   programs.fish.enable = true;
-  programs.bash.interactiveShellInit = ''
-    if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-    then
-      shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-      exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-    fi
-  '';
 }
