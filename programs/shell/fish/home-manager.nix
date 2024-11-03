f3064d69c3f50be5c429635857c56d26bc254f60
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-03 14:39:05 +0100

    fix eval

--- programs/shell/fish/home-manager.nix
+++ programs/shell/fish/home-manager.nix
@@ -1,8 +1,13 @@
 {pkgs, ...}: {
-  programs.fish.enable = true;
-  programs.fish.plugins = with pkgs.fishPlugins; [
-    tide
-  ];
+  programs.fish = {
+    enable = true;
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "tide";
+        src = tide;
+      }
+    ];
+  };
   home.persistence.default.files = [
     ".local/share/fish/fish_history"
   ];
