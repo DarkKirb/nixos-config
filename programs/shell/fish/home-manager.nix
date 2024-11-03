c41810c9727bda5a0bd27505d4fc2de0cada6ae8
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-03 15:53:24 +0100

    add autopair

--- programs/shell/fish/home-manager.nix
+++ programs/shell/fish/home-manager.nix
@@ -1,10 +1,16 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./tide.nix
     ./z.nix
   ];
   programs.fish = {
     enable = true;
+    plugins = with pkgs.fishPlugins; [
+      {
+        name = "autopair";
+        src = autopair.src;
+      }
+    ];
   };
   home.persistence.default.files = [
     ".local/share/fish/fish_history"
