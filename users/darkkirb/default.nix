88ca6654071e3861c886d999579d425a021de597
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-05 09:15:12 +0100

    fix eval

--- users/darkkirb/default.nix
+++ users/darkkirb/default.nix
@@ -1,6 +1,7 @@
 {config, ...}: {
   users.users.darkkirb = {
     createHome = true;
+    isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIDXQlfvRUm/z6eP1EjsajIbMibkq9n+ymlbBi7NFiOuaAAAABHNzaDo= ssh:"
     ];
