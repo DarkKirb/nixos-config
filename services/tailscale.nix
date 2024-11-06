5df99ba01dac722934b5f9bb2f9201c6988e4919
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-06 09:08:58 +0100

    disable tailscale on installers

--- services/tailscale.nix
+++ services/tailscale.nix
@@ -1,10 +1,17 @@
-{config, ...}: {
-  services.tailscale = {
-    enable = true;
-    authKeyFile = config.sops.secrets."services/tailscale/authKey".path;
+{
+  config,
+  lib,
+  ...
+}:
+with lib; {
+  config = mkIf (!config.isInstaller) {
+    services.tailscale = {
+      enable = true;
+      authKeyFile = config.sops.secrets."services/tailscale/authKey".path;
+    };
+    sops.secrets."services/tailscale/authKey".sopsFile = ./tailscale.yaml;
+    environment.persistence."/persistent".directories = [
+      "/var/lib/tailscale"
+    ];
   };
-  sops.secrets."services/tailscale/authKey".sopsFile = ./tailscale.yaml;
-  environment.persistence."/persistent".directories = [
-    "/var/lib/tailscale"
-  ];
 }
