daeb5b69f48aec03ebcb1e93c9ed6094eb62d5b4
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-07 08:57:31 +0100

    remove tabs from titlebar

--- programs/desktop/firefox/default.nix
+++ programs/desktop/firefox/default.nix
@@ -106,21 +106,22 @@ in {
       settings = {
         "extensions.autoDisableScopes" = 0;
         "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+        "browser.tabs.inTitlebar" = 0;
       };
       userChrome = ''
         @namespace url("http://www.mozilla.org/keymaster/gatekeeper/there.is.only.xul");
-        #main-window #titlebar {
-          overflow: hidden;
-          transition: height 0.3s 0.3s !important;
+
+        #TabsToolbar {
+          visibility: collapse;
         }
-        /* Default state: Set initial height to enable animation */
-        #main-window #titlebar { height: 3em !important; }
-        #main-window[uidensity="touch"] #titlebar { height: 3.35em !important; }
-        #main-window[uidensity="compact"] #titlebar { height: 2.7em !important; }
-        /* Hidden state: Hide native tabs strip */
-        #main-window[titlepreface*="​"] #titlebar { height: 0 !important; }
-        /* Hidden state: Fix z-index of active pinned tabs */
-        #main-window[titlepreface*="​"] #tabbrowser-tabs { z-index: 0 !important; }
+
+        #titlebar {
+          display: none;
+        }
+
+        #sidebar-header {
+          display: none;
+        }
       '';
     };
   };
