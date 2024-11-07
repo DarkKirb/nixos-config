5e8cd081a90c0dc517a26a5adcc33480db21fed4
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-07 10:02:46 +0100

    disable warnings are errors

--- config/documentation.nix
+++ config/documentation.nix
@@ -1,5 +1,6 @@
 {...}: {
   documentation.nixos.includeAllModules = true;
+  documentation.nixos.options.warningsAreErrors = false;
   home-manager.users.darkkirb.manual = {
     html.enable = true;
     json.enable = true;
