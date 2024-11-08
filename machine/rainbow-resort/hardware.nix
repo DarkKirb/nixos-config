ec66f9013719818c2c115e322d45fa4b223b8a86
Charlotte Delenk <lotte@chir.rs>
2024-11-08 17:46:23 +0100

    add radv and amdvlk

--- machine/rainbow-resort/hardware.nix
+++ machine/rainbow-resort/hardware.nix
@@ -43,5 +43,14 @@
     "gccarch-skylake-avx512"
     "ca-derivations"
   ];
+  hardware.opengl = {
+    ## radv: an open-source Vulkan driver from freedesktop
+    driSupport = true;
+    driSupport32Bit = true;
+
+    ## amdvlk: an open-source Vulkan driver from AMD
+    extraPackages = [ pkgs.amdvlk ];
+    extraPackages32 = [ pkgs.driversi686Linux.amdvlk ];
+  };
 
 }
