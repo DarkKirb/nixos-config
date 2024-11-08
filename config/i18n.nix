fe6c96345dd97d513572226b7bb60486c54fdaaf
Charlotte Delenk <lotte@chir.rs>
2024-11-08 14:18:36 +0100

    use german locale for everything except language and numbers

--- config/i18n.nix
+++ config/i18n.nix
@@ -1,3 +1,11 @@
 {config, ...}: {
   console.keyMap = "neo";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS="de_DE.UTF-8";
+    LC_MONETARY="de_DE.UTF-8";
+    LC_NAME="de_DE.UTF-8";
+    LC_PAPER="de_DE.UTF-8";
+    LC_TELEPHONE="de_DE.UTF-8";
+    LC_TIME="de_DE.UTF-8";
+  };
 }
