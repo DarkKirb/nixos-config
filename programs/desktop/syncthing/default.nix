9e1186dd7c21677a8b80023f59997807290b0193
Charlotte Delenk <lotte@chir.rs>
2024-11-08 14:08:07 +0100

    remove dumb syncthing secret

--- programs/desktop/syncthing/default.nix
+++ programs/desktop/syncthing/default.nix
@@ -3,22 +3,5 @@
     enable = true;
     tray.enable = true;
   };
-
-  sops.secrets.".local/share/syncthing/cert.pem" = {
-    sopsFile = ./${systemConfig.networking.hostName}.yaml;
-    path = "${config.xdg.dataHome}/syncthing/cert.pem";
-  };
-  sops.secrets.".local/share/syncthing/https-cert.pem" = {
-    sopsFile = ./${systemConfig.networking.hostName}.yaml;
-    path = "${config.xdg.dataHome}/syncthing/https-cert.pem";
-  };
-  sops.secrets.".local/share/syncthing/key.pem" = {
-    sopsFile = ./${systemConfig.networking.hostName}.yaml;
-    path = "${config.xdg.dataHome}/syncthing/key.pem";
-  };
-  sops.secrets.".local/share/syncthing/https-key.pem" = {
-    sopsFile = ./${systemConfig.networking.hostName}.yaml;
-    path = "${config.xdg.dataHome}/syncthing/https-key.pem";
-  };
   home.persistence.default.directories = ["${config.xdg.dataHome}/syncthing"];
 }
