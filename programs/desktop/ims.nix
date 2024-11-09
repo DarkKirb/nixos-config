483f0dd5c4eb75d6e1b8e6938bf7ebea53bb47f4
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-09 07:58:47 +0100

    put telegram cache in cache directory

--- programs/desktop/ims.nix
+++ programs/desktop/ims.nix
@@ -3,4 +3,10 @@
     telegram-desktop
   ];
   home.persistence.default.directories = [".local/share/TelegramDesktop"];
+  systemd.user.tmpfiles.rules = [
+    "d ${config.xdg.cacheHome}/TelegramDesktop/cache - - - - -"
+    "d ${config.xdg.cacheHome}/TelegramDesktop/media_cache - - - - -"
+    "L ${config.xdg.dataHome}/TelegramDesktop/tdata/user_data/cache - - - - ${config.xdg.cacheHome}/TelegramDesktop/cache"
+    "L ${config.xdg.dataHome}/TelegramDesktop/tdata/user_data/media_cache - - - - ${config.xdg.cacheHome}/TelegramDesktop/media_cache"
+  ];
 }
