eead2d9c2939616c38453c480e5b6cc99c212b94
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-09 08:57:31 +0100

    add keepassxc configuration

--- programs/desktop/password-manager.nix
+++ programs/desktop/password-manager.nix
@@ -1,5 +1,13 @@
-{ pkgs, ... }: {
+{ lib, ... }: {
   home.packages = with pkgs; [
     keepassxc
   ];
+  xdg.configFile."keepassxc.ini" = lib.generators.toINI {} {
+    General.ConfigVersion = 2;
+    Browser = {
+      CustomProxyLocation = "";
+      Enabled = true;
+      UpdateBinaryPath = false;
+    };
+  };
 }
