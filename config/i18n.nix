40cf5adf92d1f864e09f2d01009b2af6190dd79c
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-10 13:08:04 +0100

    ok but why

--- config/i18n.nix
+++ config/i18n.nix
@@ -10,8 +10,8 @@
     LC_TIME = "de_DE.UTF-8";
   };
   i18n.supportedLocales = [
-    "C/UTF-8"
-    "en_US/UTF-8"
-    "de_DE/UTF-8"
+    "C.UTF-8/UTF-8"
+    "en_US.UTF-8/UTF-8"
+    "de_DE.UTF-8/UTF-8"
   ];
 }
