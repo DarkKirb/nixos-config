4d0711b846681be3eb7a3236db450c53792c508a
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-11 08:57:40 +0100

    add calendar syncing

---
+++ programs/desktop/pim/calendar.nix
@@ -0,0 +1,22 @@
+{ pkgs, config, ... }:
+{
+  accounts.calendar.accounts.lotte = {
+    khal = {
+      enable = true;
+      addresses = [ "lotte@chir.rs" ];
+    };
+    primary = true;
+    remote = {
+      passwordCommand = [
+        "${pkgs.coreutils}/bin/cat"
+        config.sops.secrets."accounts/calendar/accounts/lotte/remote/password".path
+      ];
+      type = "caldav";
+      url = "https://calendar.zoho.eu/caldav/423167e221264cf4af974b9faa0abc3b/events/";
+      userName = "lotte@chir.rs";
+      vdirsyncer.enable = true;
+    };
+  };
+  accounts.calendar.basePaths = "Data/.calendar";
+  sops.secrets."accounts/calendar/accounts/lotte/remote/password".sopsFile = ./secrets.yaml;
+}
