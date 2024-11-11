f43f7ae8ff5d15555558a390154f0fcf8497a67a
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-11 08:45:56 +0100

    use keys.yaml

--- users/darkkirb/home-manager/sops.nix
+++ users/darkkirb/home-manager/sops.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   sops.secrets.".config/sops/age/keys.txt" = {
-    sopsFile = ./keys.txt;
+    sopsFile = ./keys.yaml;
     path = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
   };
 }
