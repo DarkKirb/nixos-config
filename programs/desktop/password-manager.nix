aebd8cb5bed9a59d1ea7a376cef0adcc7825677d
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-12 08:16:46 +0100

    fix keepassxc config path

--- programs/desktop/password-manager.nix
+++ programs/desktop/password-manager.nix
@@ -3,7 +3,7 @@
   home.packages = with pkgs; [
     keepassxc
   ];
-  xdg.configFile."keepassxc.ini".text = lib.generators.toINI { } {
+  xdg.configFile."keepassxc/keepassxc.ini".text = lib.generators.toINI { } {
     General.ConfigVersion = 2;
     Browser = {
       CustomProxyLocation = "";
