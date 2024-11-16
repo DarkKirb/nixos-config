f62e92ff98714b4e774092bcaeefe9748fce81b6
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-16 09:03:08 +0100

    don’t use lix for nixos-option

--- machine/not522/cross-packages.nix
+++ machine/not522/cross-packages.nix
@@ -10,11 +10,16 @@ let
     crossSystem.system = "riscv64-linux";
     overlays = [ lix.overlays.default ];
   };
+  pkgs_x86_64_2 = import nixpkgs {
+    system = "x86_64-linux";
+    crossSystem.system = "riscv64-linux";
+  };
 in
 {
   nixpkgs.overlays = [
     (self: super: {
-      inherit (pkgs_x86_64) lix nixos-option;
+      inherit (pkgs_x86_64) lix;
+      inherit (pkgs_x86_64_2) nixos-option;
     })
   ];
 }
