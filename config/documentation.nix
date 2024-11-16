21cbdc46517b9f077954a946d0b70c911ada5d94
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-16 09:43:04 +0100

    disable home-manager manual if the system and home-manager versions mismatch

--- config/documentation.nix
+++ config/documentation.nix
@@ -2,8 +2,17 @@
 {
   documentation.nixos.includeAllModules = true;
   documentation.nixos.options.warningsAreErrors = false;
-  home-manager.users.darkkirb.manual = {
-    html.enable = true;
-    json.enable = true;
-  };
+  home-manager.users.darkkirb =
+    {
+      lib,
+      config,
+      systemConfig,
+      ...
+    }:
+    {
+      manual = lib.mkIf (config.home.version.release == systemConfig.system.nixos.release) {
+        html.enable = true;
+        json.enable = true;
+      };
+    };
 }
