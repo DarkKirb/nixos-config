098f9a4440aff3592c84a9468c5ba118ccfcb4ed
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-17 11:56:36 +0100

    make boots quiet

--- machine/rainbow-resort/default.nix
+++ machine/rainbow-resort/default.nix
@@ -11,29 +11,13 @@
     ./hardware.nix
     "${nixos-config}/config/networkmanager.nix"
     "${nixos-config}/config/graphical.nix"
+    "${nixos-config}/config/graphical/plymouth.nix"
   ];
   system.stateVersion = "24.11";
-  specialisation.quiet = {
-    configuration.imports = [
-      "${nixos-config}/config/graphical/plymouth.nix"
-      {
-        nix.auto-update.specialisation = "quiet";
-      }
-    ];
-  };
   specialisation.sfw = {
     configuration.imports = [
       {
         nix.auto-update.specialisation = "sfw";
-        isNSFW = lib.mkForce false;
-      }
-    ];
-  };
-  specialisation.quiet-sfw = {
-    configuration.imports = [
-      "${nixos-config}/config/graphical/plymouth.nix"
-      {
-        nix.auto-update.specialisation = "quiet-sfw";
         isNSFW = lib.mkForce false;
       }
     ];
--- machine/thinkrac/default.nix
+++ machine/thinkrac/default.nix
@@ -10,29 +10,13 @@
     ./hardware.nix
     "${nixos-config}/config/networkmanager.nix"
     "${nixos-config}/config/graphical.nix"
+    "${nixos-config}/config/graphical/plymouth.nix"
   ];
   system.stateVersion = "24.11";
-  specialisation.quiet = {
-    configuration.imports = [
-      "${nixos-config}/config/graphical/plymouth.nix"
-      {
-        nix.auto-update.specialisation = "quiet";
-      }
-    ];
-  };
   specialisation.nsfw = {
     configuration.imports = [
       {
         nix.auto-update.specialisation = "nsfw";
-        isNSFW = true;
-      }
-    ];
-  };
-  specialisation.quiet-nsfw = {
-    configuration.imports = [
-      "${nixos-config}/config/graphical/plymouth.nix"
-      {
-        nix.auto-update.specialisation = "quiet-nsfw";
         isNSFW = true;
       }
     ];
