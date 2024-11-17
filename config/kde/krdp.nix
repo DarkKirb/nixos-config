a5f5f553c083ea89230461840ff4276d6cf62711
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-17 11:38:30 +0100

    fix eval

--- config/kde/krdp.nix
+++ config/kde/krdp.nix
@@ -2,8 +2,8 @@
 {
   programs.plasma.configFile.krdpserverrc.General = {
     Autostart = true;
-    Certificate = config.sops.secrets.".local/share/krdpserver/krdp.crt".file;
-    CertificateKey = config.sops.secrets.".local/share/krdpserver/krdp.key".file;
+    Certificate = config.sops.secrets.".local/share/krdpserver/krdp.crt".path;
+    CertificateKey = config.sops.secrets.".local/share/krdpserver/krdp.key".path;
     Users = "darkkirb";
   };
   sops.secrets.".local/share/krdpserver/krdp.key".sopsFile = ./krdp.yaml;
