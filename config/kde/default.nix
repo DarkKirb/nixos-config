b6c9ce5c268e4a4717786eaee476ac806ee1fc54
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-19 10:38:08 +0100

    use kwin-wayland for sddm

--- config/kde/default.nix
+++ config/kde/default.nix
@@ -1,7 +1,11 @@
 { ... }:
 {
   services.xserver.enable = true;
-  services.displayManager.sddm.enable = true;
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+    wayland.compositor = "kwin";
+  };
   services.desktopManager.plasma6.enable = true;
 
   imports = [
