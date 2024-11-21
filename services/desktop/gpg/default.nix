5f9d82aa026d3d5d8620731231e09aff5a42cdb7
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-21 09:22:59 +0100

    support gpg

--- services/desktop/gpg/default.nix
+++ services/desktop/gpg/default.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 {
   programs.gpg = {
     enable = true;
@@ -6,10 +11,29 @@
     mutableKeys = false;
     mutableTrust = false;
     scdaemonSettings.disable-ccid = true;
+    publicKeys = [
+      {
+        source = ./keys/0xB4E3D4801C49EC5E.asc;
+        trust = "ultimate";
+      }
+    ];
   };
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
     pinentryPackage = pkgs.pinentry-qt;
   };
+  sops.secrets."pgp/0xB4E3D4801C49EC5E.asc".sopsFile = ./privkey.yaml;
+  home.activation.import-gpg-privkey =
+    lib.hm.dag.entryAfter
+      [
+        "writeBoundary"
+        "sops-nix"
+        "importGpgKeys"
+      ]
+      ''
+        run env GNUPGHOME=${config.programs.gpg.homedir} ${config.programs.gpg.package}/bin/gpg --import ${
+          config.sops.secrets."pgp/0xB4E3D4801C49EC5E.asc".path
+        }
+      '';
 }
