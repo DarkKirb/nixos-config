459ecf9c604a59f5bfb8c861a4b7411acb2baaf5
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-21 08:04:51 +0100

    add gpg-agent

--- services/desktop/gpg/default.nix
+++ services/desktop/gpg/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 {
   programs.gpg = {
     enable = true;
@@ -6,5 +6,10 @@
     mutableKeys = false;
     mutableTrust = false;
     scdaemonSettings.disable-ccid = true;
+  };
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+    pinentryPackage = pkgs.pinentry-qt;
   };
 }
