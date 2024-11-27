c872513c22ed9e681152596f9475d8150b442db1
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-11-27 16:05:45 +0100

    maybe actually fix the postgresql start script

--- services/postgresql/default.nix
+++ services/postgresql/default.nix
@@ -27,9 +27,9 @@
     "${config.services.postgresqlBackup.location}"
   ];
   systemd.services.postgresql.postStart = lib.mkIf config.services.postgresql.enable ''
-    for ref in ${pkgs.coreutils}/bin/cat ${
+    for ref in $(${pkgs.coreutils}/bin/cat ${
       config.sops.secrets."systemd/services/postgresql/postStart".path
-    }; do
+    }); do
       username=$(echo $ref | cut -d= -f1)
       password=$(echo $ref | cut -d= -f2)
       ${config.services.postgresql.package}/bin/psql -U postgres -c "ALTER ROLE $username WITH LOGIN PASSWORD '$password';"
