62f6cd7df07eb6b94f70f323f8967da7ed73c538
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-08 16:44:36 +0100

    video encode waypipe

--- services/desktop/waypipe.nix
+++ services/desktop/waypipe.nix
@@ -6,7 +6,7 @@
       Unit.Description = "Runs waypipe on startup to support SSH forwarding";
       Service = {
         ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} %h/.local/state/waypipe -p";
-        ExecStart = "${lib.getExe pkgs.waypipe} --socket %h/.local/state/waypipe/client.sock client";
+        ExecStart = "${lib.getExe pkgs.waypipe} --video=bpf=8000000 --socket %h/.local/state/waypipe/client.sock client";
         ExecStopPost = "${lib.getExe' pkgs.coreutils "rm"} -f %h/.local/state/waypipe/client.sock";
       };
       Install.WantedBy = [ "graphical-session.target" ];
@@ -16,7 +16,7 @@
       Service = {
         Type = "simple";
         ExecStartPre = "${lib.getExe' pkgs.coreutils "mkdir"} %h/.local/state/waypipe -p";
-        ExecStart = "${lib.getExe pkgs.waypipe} --socket %h/.local/state/waypipe/server.sock --title-prefix '[%H] ' --login-shell --display wayland-waypipe server -- ${lib.getExe' pkgs.coreutils "sleep"} infinity";
+        ExecStart = "${lib.getExe pkgs.waypipe} --video=bpf=8000000 --socket %h/.local/state/waypipe/server.sock --title-prefix '[%H] ' --login-shell --display wayland-waypipe server -- ${lib.getExe' pkgs.coreutils "sleep"} infinity";
         ExecStopPost = "${lib.getExe' pkgs.coreutils "rm"} -f %h/.local/state/waypipe/server.sock %t/wayland-waypipe";
       };
       Install.WantedBy = [ "default.target" ];
