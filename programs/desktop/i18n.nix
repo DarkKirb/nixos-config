4cdb8a9c367c6cef85f4036f3004d48c6d68cbcd
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-09 19:45:47 +0100

    add fcitx config file

--- programs/desktop/i18n.nix
+++ programs/desktop/i18n.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 {
   i18n.inputMethod = {
     enabled = "fcitx5";
@@ -7,5 +7,23 @@
       fcitx5-chinese-addons # Needed for table inputs: https://lotte.chir.rs/kb/linux/input/table/
       fcitx5-table-extra
     ];
+  };
+  xdg.configFile."fcitx5/config".text = lib.generators.toINI { } {
+    "Groups/0" = {
+      Name = "Default";
+      "Default Layout" = "de-neo";
+      DefaultIM = "anthy";
+    };
+    "Groups/0/Items/0" = {
+      Name = "keyboard-de-neo";
+      Layout = "";
+    };
+    "Groups/0/Items/1" = {
+      Name = "anthy";
+      Layout = "";
+    };
+    GroupOrder = {
+      "0" = "default";
+    };
   };
 }
