8a3681c61778bc949ba201ca66437a9e717bc025
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-12 14:00:09 +0100

    move ollama persist to private

--- services/ollama.nix
+++ services/ollama.nix
@@ -7,5 +7,5 @@
     environmentVariables.HCC_AMDGPU_TARGET = "gfx1100";
     rocmOverrideGfx = "11.0.0";
   };
-  environment.persistence."/persistent".directories = [ config.services.ollama.home ];
+  environment.persistence."/persistent".directories = [ "/var/lib/private/ollama" ];
 }
