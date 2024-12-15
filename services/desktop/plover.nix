3c8ee7a6a6572dbfc2514fba5b96c834f2b72ec9
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-15 12:02:03 +0100

    add individual plover commands

--- services/desktop/plover.nix
+++ services/desktop/plover.nix
@@ -20,9 +20,25 @@ let
         ''[["#", ["X3", "X4", "X5", "X6", "X7", "X8", "X15", "X43", "X44", "X45", "X46", "X47", "X48"]], ["S-", ["X23"]], ["T-", ["X14"]], ["K-", ["X22"]], ["P-", ["X13"]], ["W-", ["X21"]], ["H-", ["X12"]], ["R-", ["X20"]], ["A-", ["X36"]], ["O-", ["X35"]], ["*", ["X11", "X19", "X56", "X64"]], ["-E", ["X80"]], ["-U", ["X79"]], ["-F", ["X55"]], ["-R", ["X63"]], ["-P", ["X54"]], ["-B", ["X62"]], ["-L", ["X53"]], ["-G", ["X61"]], ["-T", ["X52"]], ["-S", ["X60"]], ["-D", ["X51"]], ["-Z", ["X59"]], ["no-op", ["X1", "X2", "X9", "X10", "X17", "X18", "X25", "X26", "X27", "X28", "X29", "X30", "X31", "X32", "X33", "X34", "X39", "X40", "X41", "X42", "X49", "X50", "X57", "X58", "X65", "X66", "X67", "X68", "X69", "X70", "X71", "X72", "X73", "X74", "X75", "X76"]]]'';
     }
   );
+  plover = pkgs.writeScriptBin "plover" ''
+    #!/bin/sh
+    exec ${pkgs.plover-env}/bin/plover "$@"
+  '';
+  plover_plugins = pkgs.writeScriptBin "plover_plugins" ''
+    #!/bin/sh
+    exec ${pkgs.plover-env}/bin/plover_plugins "$@"
+  '';
+  plover_send_command = pkgs.writeScriptBin "plover_send_command" ''
+    #!/bin/sh
+    exec ${pkgs.plover-env}/bin/plover_send_command "$@"
+  '';
 in
 {
-  home.packages = [ pkgs.plover-env ];
+  home.packages = [
+    plover
+    plover_plugins
+    plover_send_command
+  ];
   home.activation.ploverSettings = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     run mkdir $VERBOSE_ARG -p $HOME/.config/plover
     run rm -f $VERBOSE_ARG $HOME/.config/plover/plover.cfg
