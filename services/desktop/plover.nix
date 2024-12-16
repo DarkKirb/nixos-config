bfc49845535854e7783ae8df7f497528bf49f881
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-16 12:35:15 +0100

    correct plover configuration

--- services/desktop/plover.nix
+++ services/desktop/plover.nix
@@ -4,19 +4,58 @@
   ...
 }:
 let
+  dictionaries = [
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/abby-left-hand-modifiers.py";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/emily-modifiers.py";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/jeff-phrasing.py";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-base.json";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-commands.json";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-movement.modal";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-numbers.json";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-proper-nouns.json";
+    }
+    {
+      enabled = true;
+      path = "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-uk-additions.json";
+    }
+  ];
   plover-cfg = pkgs.writeText "plover.cfg" (
     lib.generators.toINI { } {
       "Output Configuration".undo_levels = 100;
       "Logging Configuration".log_file = "strokes.log";
       "Translation Frame".opacity = 100;
-      Plugins.enabled_extensions = ''["RKB Unicode Sender", "modal_update", "plover_dotool_output"]'';
+      Plugins.enabled_extensions = ''["modal_update", "plover_dotool_output"]'';
       System.name = "Lapwing";
+      "System: Lapwing".dictionaries = builtins.toJSON dictionaries;
       "Machine Configuration" = {
         auto_start = "True";
-        machine_type = "TX Bolt";
+        machine_type = "Gemini PR";
       };
-      "TX Bolt" = {
-        baudrate = 8;
+      "Gemini PR" = {
+        baudrate = 1000000;
         bytesize = 8;
         parity = "N";
         port = "/dev/serial/by-id/usb-Charlotte_🦝_Delenk_rkb1-if03";
@@ -51,24 +90,4 @@ in
     run cp $VERBOSE_ARG ${plover-cfg} $HOME/.config/plover/plover.cfg
     run chmod +w $VERBOSE_ARG $HOME/.config/plover/plover.cfg
   '';
-  xdg.configFile."plover/abby-left-hand-modifiers.py".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/abby-left-hand-modifiers.py";
-  xdg.configFile."plover/emily-modifiers.py".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/emily-modifiers.py";
-  xdg.configFile."plover/emily-symbols.py".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/emily-modifiers.py";
-  xdg.configFile."plover/jeff-phrasing.py".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/jeff-phrasing.py";
-  xdg.configFile."plover/lapwing-base.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-base.json";
-  xdg.configFile."plover/lapwing-commands.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-commands.json";
-  xdg.configFile."plover/lapwing-movement.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-movement.json";
-  xdg.configFile."plover/lapwing-numbers.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-numbers.json";
-  xdg.configFile."plover/lapwing-proper-nouns.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-proper-nouns.json";
-  xdg.configFile."plover/lapwing-uk-additions.json".source =
-    "${pkgs.plover_lapwing_aio.src}/plover_lapwing/dictionaries/lapwing-uk-additions.json";
 }
