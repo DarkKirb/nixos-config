85e27302eb78efcf2996b3799c30ca0b1ea0d6f8
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-18 09:49:52 +0100

    make it use neo2

--- packages/stenography/plover_uinput/default.nix
+++ packages/stenography/plover_uinput/default.nix
@@ -17,6 +17,7 @@ buildPythonPackage {
     rm -rf dist
     substituteInPlace setup.cfg --replace-fail "xkbcommon<1.1" "xkbcommon"
     substituteInPlace plover_uinput/__init__.py --replace-fail "assert isinstance" "#"
+    substituteInPlace plover_uinput/symbols.py --replace-fail 'ctx.keymap_new_from_names(layout=layout)' 'ctx.keymap_new_from_names(layout="de", variant="neo")'
   '';
 
   propagatedBuildInputs = [
