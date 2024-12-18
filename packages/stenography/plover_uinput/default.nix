561fe44a5bb60c58586c2be9afd49cee581359a6
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-18 11:11:48 +0100

    those two layers are swapped for some reason

--- packages/stenography/plover_uinput/default.nix
+++ packages/stenography/plover_uinput/default.nix
@@ -20,7 +20,7 @@ buildPythonPackage {
       --replace-fail '"super": e.KEY_LEFTMETA,' '"super": e.KEY_LEFTMETA, "caps": e.KEY_CAPSLOCK,'
     substituteInPlace plover_uinput/symbols.py --replace-fail 'ctx.keymap_new_from_names(layout=layout)' 'ctx.keymap_new_from_names(layout="de", variant="neo")' \
       --replace-fail '2: "alt_r",' '2: "caps",' \
-      --replace-fail '3: "shift_l alt_r",' '3: "alt_r", 4: "caps shift_l", 5: "caps alt_r"'
+      --replace-fail '3: "shift_l alt_r",' '4: "alt_r", 3: "caps shift_l", 5: "caps alt_r"'
   '';
 
   propagatedBuildInputs = [
