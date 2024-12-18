879b79b235dd9a4bfc44d449c4263c9e59197c9c
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-18 10:57:42 +0100

    hack uinput into working

--- packages/stenography/plover_uinput/default.nix
+++ packages/stenography/plover_uinput/default.nix
@@ -16,8 +16,11 @@ buildPythonPackage {
     rm -rf plover_uinput.egg-info
     rm -rf dist
     substituteInPlace setup.cfg --replace-fail "xkbcommon<1.1" "xkbcommon"
-    substituteInPlace plover_uinput/__init__.py --replace-fail "assert isinstance" "#"
-    substituteInPlace plover_uinput/symbols.py --replace-fail 'ctx.keymap_new_from_names(layout=layout)' 'ctx.keymap_new_from_names(layout="de", variant="neo")'
+    substituteInPlace plover_uinput/__init__.py --replace-fail "assert isinstance" "#" \
+      --replace-fail '"super": e.KEY_LEFTMETA,' '"super": e.KEY_LEFTMETA, "caps": e.KEY_CAPSLOCK,'
+    substituteInPlace plover_uinput/symbols.py --replace-fail 'ctx.keymap_new_from_names(layout=layout)' 'ctx.keymap_new_from_names(layout="de", variant="neo")' \
+      --replace-fail '2: "alt_r",' '2: "caps",' \
+      --replace-fail '3: "shift_l alt_r",' '3: "alt_r", 4: "caps shift_l", 5: "caps alt_r"'
   '';
 
   propagatedBuildInputs = [
