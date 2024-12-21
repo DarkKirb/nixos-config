e4950992992d1031600a8bc4dd6f456064af6c03
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-21 10:55:08 +0100

    temporarily add ff14 to thinkrac

--- programs/desktop/games/ff14/default.nix
+++ programs/desktop/games/ff14/default.nix
@@ -6,10 +6,10 @@
   ...
 }:
 {
-  config = lib.mkIf (systemConfig.networking.hostName == "rainbow-resort") {
-    home.packages = with pkgs; [ xivlauncher ];
-    systemd.user.tmpfiles.rules = [
-      "L ${config.home.homeDirectory}/.xlcore - - - - ${config.home.homeDirectory}/Games/FF14/xlcore"
-    ];
-  };
+  # config = lib.mkIf (systemConfig.networking.hostName == "rainbow-resort") {
+  home.packages = with pkgs; [ xivlauncher ];
+  systemd.user.tmpfiles.rules = [
+    "L ${config.home.homeDirectory}/.xlcore - - - - ${config.home.homeDirectory}/Games/FF14/xlcore"
+  ];
+  #  };
 }
