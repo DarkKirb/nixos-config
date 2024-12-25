dff148a1e0d3673889af4d5dfc66ef4e0fe158c4
Charlotte 🦝 Delenk <lotte@chir.rs>
2024-12-25 11:08:05 +0100

    i love it when rocm is broken

--- services/ollama.nix
+++ services/ollama.nix
@@ -1,12 +1,14 @@
 { ... }:
 {
-  services.ollama = {
-    enable = true;
-    acceleration = "rocm";
-    # Thank you amd for not supporting 11.0.1
-    environmentVariables.HCC_AMDGPU_TARGET = "gfx1100";
-    rocmOverrideGfx = "11.0.0";
-    host = "[::]";
-  };
+  /*
+    services.ollama = {
+      enable = true;
+      acceleration = "rocm";
+      # Thank you amd for not supporting 11.0.1
+      environmentVariables.HCC_AMDGPU_TARGET = "gfx1100";
+      rocmOverrideGfx = "11.0.0";
+      host = "[::]";
+    };
+  */
   environment.persistence."/persistent".directories = [ "/var/lib/private/ollama" ];
 }
