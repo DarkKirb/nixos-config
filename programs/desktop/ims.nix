f151b9ce18444b91d23f2e2bdc5bc026d6d7c92d
Charlotte 🦝 Delenk <lotte@chir.rs>
2025-01-09 08:59:23 +0100

    disable telegram and element on aarch64

--- programs/desktop/ims.nix
+++ programs/desktop/ims.nix
@@ -7,30 +7,28 @@
   ...
 }:
 let
-  withDiscord = system == "x86_64-linux";
+  withElectronAndTg = system == "x86_64-linux";
 in
 {
   home.packages =
     with pkgs;
     lib.mkMerge [
-      [
+      (lib.mkIf withElectronAndTg [
         telegram-desktop
         element-desktop
-      ]
-      (lib.mkIf withDiscord [
         discord
         betterdiscordctl
       ])
     ];
   home.persistence.default.directories = lib.mkMerge [
-    [
+    (lib.mkIf withElectronAndTg [
+      ".local/share/discord"
       ".local/share/TelegramDesktop"
       ".local/share/Element"
-    ]
-    (lib.mkIf withDiscord [ ".local/share/discord" ])
+    ])
   ];
   systemd.user.tmpfiles.rules = lib.mkMerge [
-    [
+    (lib.mkIf withElectronAndTg [
       "d /persistent${config.xdg.cacheHome}/TelegramDesktop/cache - - - - -"
       "d /persistent${config.xdg.cacheHome}/TelegramDesktop/media_cache - - - - -"
       "d /persistent${config.xdg.cacheHome}/Element/Cache - - - - -"
@@ -43,8 +41,6 @@ in
       "L /persistent${config.xdg.dataHome}/Element/Code\\x20Cache - - - - ${config.xdg.cacheHome}/Element/Code\\x20Cache"
       "L /persistent${config.xdg.dataHome}/Element/DawnGraphiteCache - - - - ${config.xdg.cacheHome}/Element/DawnGraphiteCache"
       "L ${config.xdg.configHome}/Element - - - - ${config.xdg.dataHome}/Element"
-    ]
-    (lib.mkIf withDiscord [
       "d /persistent${config.xdg.cacheHome}/discord - - - - -"
       "d /persistent${config.xdg.cacheHome}/discord/Cache - - - - -"
       "d /persistent${config.xdg.cacheHome}/discord/Code\\x20Cache - - - - -"
@@ -58,34 +54,28 @@ in
       "L /persistent${config.xdg.dataHome}/discord/userDataCache.json - - - - ${config.xdg.cacheHome}/discord/userDataCache.json"
       "L ${config.xdg.configHome}/discord - - - - ${config.xdg.dataHome}/discord"
     ])
-    # GPU Cache sometimes breaks for electron apps on intel, so only persist that on non-intel
-    (lib.mkIf (!systemConfig.isIntelGPU) [
+    (lib.mkIf (!systemConfig.isIntelGPU && withElectronAndTg) [
       "d /persistent${config.xdg.cacheHome}/Element/DawnWebGPUCache - - - - -"
       "d /persistent${config.xdg.cacheHome}/Element/GPUCache - - - - -"
       "L /persistent${config.xdg.dataHome}/Element/DawnWebGPUCache - - - - ${config.xdg.cacheHome}/Element/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/Element/GPUCache - - - - ${config.xdg.cacheHome}/Element/GPUCache"
-    ])
-    (lib.mkIf (!systemConfig.isIntelGPU && withDiscord) [
       "d /persistent${config.xdg.cacheHome}/discord/DawnWebGPUCache - - - - -"
       "d /persistent${config.xdg.cacheHome}/discord/GPUCache - - - - -"
       "L /persistent${config.xdg.dataHome}/discord/DawnWebGPUCache - - - - ${config.xdg.cacheHome}/discord/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/discord/GPUCache - - - - ${config.xdg.cacheHome}/discord/GPUCache"
     ])
-    (lib.mkIf (systemConfig.isIntelGPU) [
+    (lib.mkIf (systemConfig.isIntelGPU && withElectronAndTg) [
       "d /tmp${config.xdg.cacheHome}/Element/DawnWebGPUCache - - - - -"
       "d /tmp${config.xdg.cacheHome}/Element/GPUCache - - - - -"
-
       "L /persistent${config.xdg.dataHome}/Element/DawnWebGPUCache - - - - /tmp${config.xdg.cacheHome}/Element/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/Element/GPUCache - - - - /tmp${config.xdg.cacheHome}/Element/GPUCache"
-    ])
-    (lib.mkIf (systemConfig.isIntelGPU && withDiscord) [
       "d /tmp${config.xdg.cacheHome}/discord/DawnWebGPUCache - - - - -"
       "d /tmp${config.xdg.cacheHome}/discord/GPUCache - - - - -"
       "L /persistent${config.xdg.dataHome}/discord/DawnWebGPUCache - - - - /tmp${config.xdg.cacheHome}/discord/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/discord/GPUCache - - - - /tmp${config.xdg.cacheHome}/discord/GPUCache"
     ])
   ];
-  systemd.user.services.betterdiscord = lib.mkIf withDiscord {
+  systemd.user.services.betterdiscord = lib.mkIf withElectronAndTg {
     Unit = {
       Description = "Patch discord";
       After = [ "graphical-session.target" ];
