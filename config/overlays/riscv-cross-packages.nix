562d1511fc7ba066752e84bd9943fb45490c8fb1
Charlotte 🦝 Delenk <lotte@chir.rs>
2025-01-19 18:42:29 +0100

    disable tpm2 tss in systemd

--- config/overlays/riscv-cross-packages.nix
+++ config/overlays/riscv-cross-packages.nix
@@ -30,6 +30,9 @@ in
       gnupg24 = super.gnupg24.override {
         withTpm2Tss = false;
       };
+      systemd = super.systemd.override {
+        withTpm2Tss = false;
+      };
     })
   ];
   environment.etc."nix/inputs/nixpkgs-overlays/riscv-cross-packages.nix".text = ''
