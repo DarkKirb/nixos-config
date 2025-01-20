b897f5c07f32c63b0aa5d19fa53532dd0a9d0d01
Charlotte 🦝 Delenk <lotte@chir.rs>
2025-01-20 10:57:02 +0100

    move fcitx to the plasma configuration

--- programs/desktop/i18n.nix
+++ programs/desktop/i18n.nix
@@ -1,13 +1,5 @@
-{ pkgs, lib, ... }:
+{ lib, ... }:
 {
-  i18n.inputMethod = {
-    enabled = "fcitx5";
-    fcitx5.addons = with pkgs; [
-      fcitx5-anthy
-      fcitx5-chinese-addons # Needed for table inputs: https://lotte.chir.rs/kb/linux/input/table/
-      fcitx5-table-extra
-    ];
-  };
   xdg.configFile."fcitx5/profile".text = lib.generators.toINI { } {
     "Groups/0" = {
       Name = "Default";
