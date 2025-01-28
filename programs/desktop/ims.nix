d59a21009b607ed682973b4f61a53da6faf3a428
Charlotte 🦝 Delenk <lotte@chir.rs>
2025-01-28 08:40:13 +0100

    persist signal desktop

--- programs/desktop/ims.nix
+++ programs/desktop/ims.nix
@@ -24,6 +24,7 @@ in
     (lib.mkIf withElectronAndTg [
       ".local/share/discord"
       ".local/share/TelegramDesktop"
+      ".local/share/Signal"
     ])
   ];
   systemd.user.tmpfiles.rules = lib.mkMerge [
@@ -44,18 +45,38 @@ in
       "L /persistent${config.xdg.dataHome}/discord/DawnGraphiteCache - - - - ${config.xdg.cacheHome}/discord/DawnGraphiteCache"
       "L /persistent${config.xdg.dataHome}/discord/userDataCache.json - - - - ${config.xdg.cacheHome}/discord/userDataCache.json"
       "L ${config.xdg.configHome}/discord - - - - ${config.xdg.dataHome}/discord"
+      "d /persistent${config.xdg.cacheHome}/Signal - - - - -"
+      "d /persistent${config.xdg.cacheHome}/Signal/Cache - - - - -"
+      "d /persistent${config.xdg.cacheHome}/Signal/Code\\x20Cache - - - - -"
+      "d /persistent${config.xdg.cacheHome}/Signal/component_crx_cache - - - - -"
+      "d /persistent${config.xdg.cacheHome}/Signal/DawnGraphiteCache - - - - -"
+      "d /persistent${config.xdg.dataHome}/Signal - - - - -"
+      "L /persistent${config.xdg.dataHome}/Signal/Cache - - - - ${config.xdg.cacheHome}/Signal/Cache"
+      "L /persistent${config.xdg.dataHome}/Signal/Code\\x20Cache - - - - ${config.xdg.cacheHome}/Signal/Code\\x20Cache"
+      "L /persistent${config.xdg.dataHome}/Signal/component_crx_cache - - - - ${config.xdg.cacheHome}/Signal/component_crx_cache"
+      "L /persistent${config.xdg.dataHome}/Signal/DawnGraphiteCache - - - - ${config.xdg.cacheHome}/Signal/DawnGraphiteCache"
+      "L /persistent${config.xdg.dataHome}/Signal/userDataCache.json - - - - ${config.xdg.cacheHome}/Signal/userDataCache.json"
+      "L ${config.xdg.configHome}/Signal - - - - ${config.xdg.dataHome}/Signal"
     ])
     (lib.mkIf (!systemConfig.isIntelGPU && withElectronAndTg) [
       "d /persistent${config.xdg.cacheHome}/discord/DawnWebGPUCache - - - - -"
       "d /persistent${config.xdg.cacheHome}/discord/GPUCache - - - - -"
       "L /persistent${config.xdg.dataHome}/discord/DawnWebGPUCache - - - - ${config.xdg.cacheHome}/discord/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/discord/GPUCache - - - - ${config.xdg.cacheHome}/discord/GPUCache"
+      "d /persistent${config.xdg.cacheHome}/Signal/DawnWebGPUCache - - - - -"
+      "d /persistent${config.xdg.cacheHome}/Signal/GPUCache - - - - -"
+      "L /persistent${config.xdg.dataHome}/Signal/DawnWebGPUCache - - - - ${config.xdg.cacheHome}/Signal/DawnWebGPUCache"
+      "L /persistent${config.xdg.dataHome}/Signal/GPUCache - - - - ${config.xdg.cacheHome}/Signal/GPUCache"
     ])
     (lib.mkIf (systemConfig.isIntelGPU && withElectronAndTg) [
       "d /tmp${config.xdg.cacheHome}/discord/DawnWebGPUCache - - - - -"
       "d /tmp${config.xdg.cacheHome}/discord/GPUCache - - - - -"
       "L /persistent${config.xdg.dataHome}/discord/DawnWebGPUCache - - - - /tmp${config.xdg.cacheHome}/discord/DawnWebGPUCache"
       "L /persistent${config.xdg.dataHome}/discord/GPUCache - - - - /tmp${config.xdg.cacheHome}/discord/GPUCache"
+      "d /tmp${config.xdg.cacheHome}/Signal/DawnWebGPUCache - - - - -"
+      "d /tmp${config.xdg.cacheHome}/Signal/GPUCache - - - - -"
+      "L /persistent${config.xdg.dataHome}/Signal/DawnWebGPUCache - - - - /tmp${config.xdg.cacheHome}/Signal/DawnWebGPUCache"
+      "L /persistent${config.xdg.dataHome}/Signal/GPUCache - - - - /tmp${config.xdg.cacheHome}/Signal/GPUCache"
     ])
   ];
   systemd.user.services.betterdiscord = lib.mkIf withElectronAndTg {
