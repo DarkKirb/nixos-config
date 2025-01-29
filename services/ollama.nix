31a6b48c840801c7d0ef2257ffdba55e0593d791
Charlotte 🦝 Delenk <lotte@chir.rs>
2025-01-29 08:37:33 +0100

    readd ollama

--- services/ollama.nix
+++ services/ollama.nix
@@ -1,14 +1,11 @@
 { ... }:
 {
-  /*
-    services.ollama = {
-      enable = true;
-      acceleration = "rocm";
-      # Thank you amd for not supporting 11.0.1
-      environmentVariables.HCC_AMDGPU_TARGET = "gfx1100";
-      rocmOverrideGfx = "11.0.0";
-      host = "[::]";
-    };
-  */
+
+  services.ollama = {
+    enable = true;
+    acceleration = "rocm";
+    host = "[::]";
+  };
+
   environment.persistence."/persistent".directories = [ "/var/lib/private/ollama" ];
 }
