e5de8eecbf013c44a5958b441193ad24491ba80b
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-01-29 15:05:54 +0100

    override amdgpu version again

--- services/ollama.nix
+++ services/ollama.nix
@@ -4,6 +4,9 @@
   services.ollama = {
     enable = true;
     acceleration = "rocm";
+    # Thank you amd for not supporting 11.0.1
+    environmentVariables.HCC_AMDGPU_TARGET = "gfx1100";
+    rocmOverrideGfx = "11.0.0";
     host = "[::]";
   };
   nixpkgs.overlays = [
