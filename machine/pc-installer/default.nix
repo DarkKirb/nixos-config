f78e6feca528890e47ae0a31d0d1938daa3be3e2
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-02-02 20:40:17 +0100

    massively reduce installer complexity

--- machine/pc-installer/default.nix
+++ machine/pc-installer/default.nix
@@ -2,29 +2,9 @@
   config,
   nixos-config,
   pkgs,
-  pureInputs,
   lib,
   ...
 }:
-let
-  dependencies = [
-    nixos-config.nixosConfigurations.rainbow-resort.config.system.build.toplevel
-    nixos-config.nixosConfigurations.rainbow-resort.config.system.build.diskoScript
-    nixos-config.nixosConfigurations.rainbow-resort.config.system.build.diskoScript.drvPath
-    nixos-config.nixosConfigurations.rainbow-resort.pkgs.stdenv.drvPath
-    (nixos-config.nixosConfigurations.rainbow-resort.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
-    nixos-config.nixosConfigurations.thinkrac.config.system.build.toplevel
-    nixos-config.nixosConfigurations.thinkrac.config.system.build.diskoScript
-    nixos-config.nixosConfigurations.thinkrac.config.system.build.diskoScript.drvPath
-    nixos-config.nixosConfigurations.thinkrac.pkgs.stdenv.drvPath
-    (nixos-config.nixosConfigurations.thinkrac.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
-    nixos-config.nixosConfigurations.nas.config.system.build.toplevel
-    nixos-config.nixosConfigurations.nas.pkgs.stdenv.drvPath
-    (nixos-config.nixosConfigurations.nas.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
-  ] ++ map (i: i.outPath) (builtins.filter builtins.isAttrs (builtins.attrValues pureInputs));
-
-  closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
-in
 {
   networking.hostName = "pc-installer";
   imports = [
@@ -36,7 +16,6 @@ in
   ];
   system.stateVersion = config.system.nixos.version;
   isInstaller = true;
-  environment.etc."install-closure".source = "${closureInfo}/store-paths";
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "install-thinkrac-unattended" ''
       set -eux
