6ac79aced4ff420c3b8e673e0d549e57fe814ab4
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-02-20 11:02:29 +0100

    switch CI to github

--- hydra/jobsets.nix
+++ hydra/jobsets.nix
@@ -1,48 +1,46 @@
 {
   prsJSON,
   nixpkgs,
-}: let
-  pkgs = import nixpkgs {};
+}:
+let
+  pkgs = import nixpkgs { };
   prs = builtins.fromJSON (builtins.readFile prsJSON);
   jobsets =
     (builtins.listToAttrs (
-      pkgs.lib.attrsets.mapAttrsToList (
-        _: info: {
-          name = "pr${toString info.number}";
-          value = {
-            enabled = info.state == "open";
-            hidden = info.state != "open";
-            description = "PR ${toString info.number}: ${info.title}";
-            nixexprinput = "nixos-config";
-            nixexprpath = "hydra/default.nix";
-            checkinterval = 3600;
-            schedulingshares = 100;
-            enableemail = false;
-            emailoverride = "";
-            keepnr = 1;
-            inputs = {
-              nixos-config = {
-                type = "git";
-                value = "${info.head.repo.clone_url} ${info.head.ref}";
-                emailresponsible = false;
-              };
-              gitea_status_repo = {
-                type = "string";
-                value = "nixos-config";
-              };
-              gitea_repo_owner = {
-                type = "string";
-                value = info.head.repo.owner.login;
-              };
-              gitea_repo_name = {
-                type = "string";
-                value = info.head.repo.name;
-              };
+      pkgs.lib.attrsets.mapAttrsToList (_: info: {
+        name = "pr${toString info.number}";
+        value = {
+          enabled = info.state == "open";
+          hidden = info.state != "open";
+          description = "PR ${toString info.number}: ${info.title}";
+          nixexprinput = "nixos-config";
+          nixexprpath = "hydra/default.nix";
+          checkinterval = 3600;
+          schedulingshares = 100;
+          enableemail = false;
+          emailoverride = "";
+          keepnr = 1;
+          inputs = {
+            nixos-config = {
+              type = "git";
+              value = "${info.head.repo.clone_url} ${info.head.ref}";
+              emailresponsible = false;
+            };
+            github_input = {
+              type = "string";
+              value = "nixos-config";
+            };
+            github_repo_owner = {
+              type = "string";
+              value = info.head.repo.owner.login;
+            };
+            github_repo_name = {
+              type = "string";
+              value = info.head.repo.name;
             };
           };
-        }
-      )
-      prs
+        };
+      }) prs
     ))
     // {
       nixos-config = {
@@ -59,22 +57,25 @@
         inputs = {
           nixos-config = {
             type = "git";
-            value = "https://git.chir.rs/darkkirb/nixos-config main";
+            value = "https://github.com/DarkKirb/nixos-config main";
             emailresponsible = false;
           };
-          gitea_status_repo = {
+          github_input = {
             type = "string";
             value = "nixos-config";
           };
-          gitea_repo_owner = {
+          github_repo_owner = {
             type = "string";
-            value = "Darkkirb";
+            value = "DarkKirb";
           };
-          gitea_repo_name = {
+          github_repo_name = {
             type = "string";
             value = "nixos-config";
           };
         };
       };
     };
-in {jobsets = pkgs.writeText "jobsets.json" (builtins.toJSON jobsets);}
+in
+{
+  jobsets = pkgs.writeText "jobsets.json" (builtins.toJSON jobsets);
+}
