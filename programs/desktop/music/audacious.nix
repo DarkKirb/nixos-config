a8e9168b38af0251817101c71117b2689a38df29
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-02-28 10:10:50 +0100

    add clscrobble to audacious

--- programs/desktop/music/audacious.nix
+++ programs/desktop/music/audacious.nix
@@ -1,7 +1,21 @@
-{ pkgs, ... }:
 {
-  home.packages = with pkgs; [ audacious ];
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  home.packages = with pkgs; [
+    audacious
+    (writeScriptBin "clscrobble" ''
+      #!${lib.getExe bash}
+      export LISTENBRAINZ_TOKEN=$(cat ${config.sops.secrets."env/LISTENBRAINZ_TOKEN".path})
+      exec clscrobble "$@"
+    '')
+  ];
   home.persistence.default.directories = [
     ".config/audacious"
   ];
+  sops.secrets."env/LISTENBRAINZ_TOKEN".sopsFile = ./listenbrainz.yaml;
+
 }
