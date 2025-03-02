1fa062c10aa4080ef6adf9af0bbd0f895cb1bd25
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-02 21:19:46 +0100

    Fix the hardware configuration of stinky-ssb

--- machine/stinky-ssb/hardware.nix
+++ machine/stinky-ssb/hardware.nix
@@ -1,9 +1,48 @@
-{ lib, pkgs, ... }:
 {
-  boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.linux-devterm);
-  boot.kernelParams = [ "fbcon=rotate:1" ];
+  lib,
+  pkgs,
+  nixos-hardware,
+  ...
+}:
+{
+  imports = [
+    "${nixos-hardware}/raspberry-pi/4"
+  ];
+  #boot.kernelPackages = lib.mkForce (pkgs.linuxPackagesFor pkgs.linux-devterm);
+  boot.kernelPackages = lib.mkForce pkgs.linuxKernel.packages.linux_rpi4;
+  boot.kernelPatches = [
+
+    {
+      name = "AXP20x clockwork pi support";
+      patch = ./patches/0002-mfd-axp20x-add-clockworkpi-a06-power-support.patch;
+    }
+
+    /*
+      {
+        name = "CWD686 drm panel";
+        patch = ./patches/0004-gpu-drm-panel-add-cwd686-driver.patch;
+        extraStructuredConfig.DRM_PANEL_CWD686 = lib.kernel.module;
+      }
+    */
+    {
+      name = "CWD686 drm panel";
+      patch = ./patches/0003-cwd686.patch;
+      extraStructuredConfig.DRM_PANEL_CLOCKWORKPI_CWD686 = lib.kernel.module;
+    }
+    {
+      name = "OCP8178 backlight";
+      patch = ./patches/0005-video-backlight-add-ocp8178-driver.patch;
+      extraStructuredConfig.BACKLIGHT_OCP8178 = lib.kernel.module;
+    }
+  ];
+  boot.kernelParams = [
+    "fbcon=rotate:1"
+    "earlycon=uart8250,mmio32,0xfe215040"
+    "console=serial0,115200"
+    "8250.nr_uarts=1"
+  ];
   boot.initrd = {
-    includeDefaultModules = false;
+    #includeDefaultModules = false;
     availableKernelModules = [
       "usbhid"
       "usb_storage"
@@ -13,7 +52,7 @@
       "mmc_block"
       "usbhid"
       "hid_generic"
-      "panel_cwd686"
+      "panel_clockworkpi_cwd686"
       "ocp8178_bl"
       "ti_adc081c"
     ];
@@ -23,12 +62,23 @@
     raspberrypi-eeprom
   ];
   hardware.deviceTree.name = "broadcom/bcm2711-rpi-cm4.dtb";
-  hardware.deviceTree.filter = "*rpi*.dtb";
+  hardware.deviceTree.filter = "bcm2711-rpi-cm4.dtb";
+  hardware.raspberry-pi."4" = {
+    #audio.enable = true;
+    dwc2 = {
+      enable = true;
+      dr_mode = "host";
+    };
+    apply-overlays-dtmerge.enable = true;
+  };
+
   hardware.deviceTree.overlays = [
-    {
-      name = "dwc2";
-      dtsFile = ./dts/dwc2-overlay.dts;
-    }
+    /*
+      {
+        name = "dwc2";
+        dtsFile = ./dts/dwc2-overlay.dts;
+      }
+    */
     {
       name = "cma";
       dtsFile = ./dts/cma-overlay.dts;
@@ -53,14 +103,6 @@
       name = "audremap";
       dtsFile = ./dts/audremap-overlay.dts;
     }
-    {
-      name = "spi";
-      dtsFile = ./dts/spi0-overlay.dts;
-    }
-    {
-      name = "devterm-overlay";
-      dtsFile = ./dts/devterm-overlay.dts;
-    }
   ];
   services.xserver.xkb.variant = lib.mkForce "us";
   console.keyMap = lib.mkForce "us";
@@ -79,4 +121,8 @@
   };
   boot.initrd.systemd.tpm2.enable = lib.mkForce false;
   systemd.tpm2.enable = lib.mkForce false;
+  services.xserver.videoDrivers = lib.mkBefore [
+    "modesetting" # Prefer the modesetting driver in X11
+    "fbdev" # Fallback to fbdev
+  ];
 }
