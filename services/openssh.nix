cffc76c738b92ccc40bede0d968c49ef1c61e54e
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-09 08:07:19 +0100

    add github known host

--- services/openssh.nix
+++ services/openssh.nix
@@ -21,6 +21,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILpOcmvVODaja3wDdnocb/k6MK7vsh5uH8gpeHR9+/rY";
     "rainbow-resort.int.chir.rs".publicKey =
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAII9MczPuvEh9XaT6e3emfC+WyMYEpyRu2jDUkt3bBk8W";
+    "github.com".publicKey =
+      "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=";
   };
   networking.firewall.allowedTCPPorts = [ 22 ];
 }
