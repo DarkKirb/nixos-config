c66dbb40a010e5904b76e3d573248244f0242c81
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-11 19:35:29 +0100

    remove kodi patch

--- overlays/workarounds.nix
+++ overlays/workarounds.nix
@@ -4,11 +4,4 @@ final: prev: {
       ./fcitx-table-extra/sitelen-pona.patch
     ];
   });
-  kodi = prev.kodi.override {
-    curl = final.curl.overrideAttrs (super: {
-      patches = super.patches or [ ] ++ [
-        ./curl/curl-fix-scache-bug.patch
-      ];
-    });
-  };
 }
