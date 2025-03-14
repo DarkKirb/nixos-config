7488fa618c567a914e59d87330da3a0634d87fb3
Charlotte <lotte@chir.rs>
2025-03-14 21:45:28 +0100

    Revert "remove broken nixpkgs-overlay entry"

--- config/overlays/default.nix
+++ config/overlays/default.nix
@@ -7,9 +7,7 @@
   nixpkgs.overlays = [
     nixos-config.overlays.default
   ];
-  /*
-    environment.etc."nix/inputs/nixpkgs-overlays/default.nix".text = ''
-      (import <nixpkgs/lib>).composeManyExtensions (import <nixos-config>).outputs.nixosConfigurations.${config.networking.hostName}.pkgs.overlays
-    '';
-  */
+  environment.etc."nix/inputs/nixpkgs-overlays/default.nix".text = ''
+    (import <nixpkgs/lib>).composeManyExtensions (import <nixos-config>).outputs.nixosConfigurations.${config.networking.hostName}.pkgs.overlays
+  '';
 }
