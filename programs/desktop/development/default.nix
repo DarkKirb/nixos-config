e2add42d44648b0e90ffa59173eed2bda91b49e2
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-21 19:00:12 +0100

    add kubectl config

--- programs/desktop/development/default.nix
+++ programs/desktop/development/default.nix
@@ -1,7 +1,11 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   imports = [ ./rust ];
   home.packages = with pkgs; [
     (python3.withPackages (_: [ ]))
+    kubectl
+    kubernetes-helm
   ];
+  home.sessionVariables.KUBECONFIG = config.sops.secrets.".config/kube/config".path;
+  sops.secrets.".config/kube/config".sopsFile = ./secrets.yaml;
 }
