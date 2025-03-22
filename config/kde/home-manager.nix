e2eb70b69ea67cc2df52121163ba302c1e92335d
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-22 17:56:46 +0100

    automatically save screenshot

--- config/kde/home-manager.nix
+++ config/kde/home-manager.nix
@@ -4,6 +4,10 @@
   programs.plasma.configFile.baloofilerc."Basic Settings"."Indexing-Enabled" = false;
   programs.plasma.configFile.kwalletrc."org.freedesktop.secrets".apiEnabled = false;
   programs.plasma.configFile.kwalletrc.Wallet.Enabled = false;
+  programs.plasma.configFile.spectaclerc = {
+    General.autoSaveImage = true;
+    General.launchAction = "UseLastUsedCapturemode";
+  };
   imports = [
     plasma-manager.homeManagerModules.plasma-manager
     ./theming.nix
