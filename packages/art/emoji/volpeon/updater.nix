a9f9a2532e0acc530e525c5157775e264af29b83
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-22 07:17:55 +0100

    sort volpeon’s json

--- packages/art/emoji/volpeon/updater.nix
+++ packages/art/emoji/volpeon/updater.nix
@@ -1,6 +1,10 @@
-{ lib, curl }:
+{
+  lib,
+  jq,
+  curl,
+}:
 ''
   echo "volpeon-emoji: Updating"
-  ${lib.getExe curl} https://volpeon.ink/emojis/pleroma.json > packages/art/emoji/volpeon/emoji.json
+  ${lib.getExe curl} https://volpeon.ink/emojis/pleroma.json | $[lib.getExe pkgs.jq} -S > packages/art/emoji/volpeon/emoji.json
   echo "volpeon-emoji: Done"
 ''
