4ecbad08a4328dce2b16021d0233054b84e58f82
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-03-28 19:06:32 +0100

    make it work

--- config/lanzaboote/default.nix
+++ config/lanzaboote/default.nix
@@ -17,36 +17,14 @@
 
   boot.lanzaboote = {
     enable = true;
-    pkiBundle = "/var/lib/sbctl";
+    pkiBundle = "/run/secrets/var/lib/sbctl";
   };
 
-  sops.secrets."var/lib/sbctl/GUID" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/GUID";
-  };
-  sops.secrets."var/lib/sbctl/keys/db/db.key" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/db/db.key";
-  };
-  sops.secrets."var/lib/sbctl/keys/db/db.pem" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/db/db.pem";
-  };
-  sops.secrets."var/lib/sbctl/keys/KEK/KEK.key" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/KEK/KEK.key";
-  };
-  sops.secrets."var/lib/sbctl/keys/KEK/KEK.pem" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/KEK/KEK.pem";
-  };
-  sops.secrets."var/lib/sbctl/keys/PK/PK.key" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/PK/PK.key";
-  };
-  sops.secrets."var/lib/sbctl/keys/PK/PK.pem" = {
-    sopsFile = ./secrets.yaml;
-    path = "/var/lib/sbctl/keys/PK/PK.pem";
-  };
-
+  sops.secrets."var/lib/sbctl/GUID".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/db/db.key".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/db/db.pem".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/KEK/KEK.key".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/KEK/KEK.pem".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/PK/PK.key".sopsFile = ./secrets.yaml;
+  sops.secrets."var/lib/sbctl/keys/PK/PK.pem".sopsFile = ./secrets.yaml;
 }
