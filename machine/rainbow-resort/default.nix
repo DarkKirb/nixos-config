8da9b8d0e8c5aa091248708ebec68aaa6d4757f6
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-04-20 09:31:23 +0100

    hotfixes

--- machine/rainbow-resort/default.nix
+++ machine/rainbow-resort/default.nix
@@ -10,6 +10,9 @@
     ../../services/chir-rs
     ../../services/ollama.nix
     ../../services/jellyfin.nix
+    ../../services/yiffstash
+    ../../services/renovate
+    ../../services/hydra
   ];
   system.stateVersion = "24.11";
   home-manager.users.darkkirb.imports = [ ./home-manager.nix ];
