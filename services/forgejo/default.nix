bfbb5463ed4e65c145a111d1c23622bd76a7e0da
Charlotte 🦝 Deleńkec <lotte@chir.rs>
2025-04-20 21:01:21 +0100

    bruh

--- services/forgejo/default.nix
+++ services/forgejo/default.nix
@@ -11,7 +11,7 @@ let
       {
         name = "renovate";
         user_agent_regex = "RenovateBot";
-        action = "allow";
+        action = "ALLOW";
       }
     ] ++ botsOrig.bots;
     inherit (botsOrig) dnsbl;
